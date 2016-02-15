ea0124a4b8dcef1bdd661ad319f67bf4293fc5a4
Simon <sims38930@gmail.com>
2016-02-15 12:39:56 +0100

    On revois le code faire entre deux séances

--- morpion/python/morpyon.py
+++ morpion/python/morpyon.py
@@ -2,9 +2,11 @@
 
 from sys import exit
 import pygame
+from pygame.locals import *
 from morpyon_game import *
+import morpyon_drawing
 
-screen_size = (640, 400)
+screen_size = (900, 900)
 red = (255, 0, 0)
 blue = (0, 0, 255)
 grey = (64, 64, 64)
@@ -20,12 +22,35 @@ for (x,y) in game.positions():
     cell = pygame.Rect((x,y), game.box_size())
     cell_surface = screen.subsurface(cell)
     cell_surface.fill(red)
+    morpyon_drawing.grille(screen)
 
-pygame.display.update()
+etat_courant = "vide"
+player = 1
+
+def croix():
+    pygame.draw.line(cell_surface, 0x0000a0, [0, 0], [213, 133], 10)
+    pygame.draw.line(cell_surface, 0x0000a0, [213, 0], [0, 133], 10)
+
+def rond():
+    pygame.draw.circle(cell_surface, 0x00000, [107, 66], 100, 10)
+
+
 while 1:
-    event = pygame.event.poll()
-    if event.type == pygame.QUIT:
-        exit(0)
+    for event in pygame.event.get():
+        if event.type == MOUSEBUTTONDOWN:
+            position = pygame.mouse.get_pos()
+            if etat_courant != "vide":
+                if player == 1:
+                    croix()
+                    player = 2
+                else:
+                    rond()
+                    player = 1
+            etat_courant = "plein"
+        if event.type == QUIT:
+            exit()
+    pygame.display.flip()
+    pygame.time.delay(100)
 
 
 
