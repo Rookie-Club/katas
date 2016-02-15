fe31cf17dbea594d13cb95302f18f555c4805a1c
Simon <sims38930@gmail.com>
2016-02-15 17:39:56 +0100

    Revue de fonction, introduction de la gestion des erreurs

--- nim/python/nim.py
+++ nim/python/nim.py
@@ -1,17 +1,31 @@
-def batons_restant(batons, baton_retirer):
-    return batons - int(baton_retirer)
+class NotAuthorizedNumber(Exception):
+    def __init__(self, baton_retirer):
+        self.message = "Vous avez retirer " + str(baton_retirer - 3) + " baton(s) de trop"
+
+    def __str__(self):
+        return repr(self.message)
+
+def raw_int_input(message):
+    try:
+        return int(raw_input(message))
+    except ValueError as error:
+        print "Vous devez saisir un nombre"
+        return raw_int_input(message)
 
 def tour_de_jeu(joueur, batons):
-    baton = raw_input("joueur " + joueur + ", combien tu enleve de baton ? ")
-    batons = batons_restant(batons, baton)
+    baton_retirer = raw_int_input("joueur " + joueur + ", combien tu enleve de baton ? ")
+    if baton_retirer > 3:
+        raise NotAuthorizedNumber(baton_retirer)
+    batons = batons - baton_retirer
     print "il reste", batons, "batons."
     return batons
 
 if __name__ == "__main__":
-    batons = int(raw_input("on fait une partie pour combien de baton ? "))
-
+    batons = raw_int_input("on fait une partie pour combien de baton ? ")
     print "on commence une partie avec", batons
-
-    batons = tour_de_jeu("1", batons)
-    batons = tour_de_jeu("2", batons)
+    try:
+        batons = tour_de_jeu("1", batons)
+        batons = tour_de_jeu("2", batons)
+    except NotAuthorizedNumber as e:
+        print e
 
