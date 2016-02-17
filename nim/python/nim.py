abb9a0ce998cbc6b760f8e0261ff73b27c3acb38
Simon <sims38930@gmail.com>
2016-02-17 11:39:56 +0100

    Reusinage avec un objet

--- nim/python/nim.py
+++ nim/python/nim.py
@@ -1,11 +1,27 @@
-batons = int(raw_input("Avec combien de batons voulez vous jouer ? "))
-print "Vous jouez avec %d batons !" % batons
+# coding=utf-8
 
-count = 0
+class NimGame():
+    def __init__(self, batons, joueurs):
+        self.batons = batons
+        self.tour_de_jeu = 0
+        self.joueurs = joueurs
 
-while batons > 0:
-    joueur = (count % 2) + 1
-    batons_retirer = int(raw_input("joueur %d combien voulez vous retirez de batons ? " % joueur))
-    batons = batons - batons_retirer
-    print "il reste %d batons" % batons
-    count += 1
+    def encore_des_batons(self):
+        return self.batons > 0
+
+    def retire(self, batons_a_retirer):
+        self.batons -= batons_a_retirer
+        self.tour_de_jeu += 1
+
+    def joueur_courant(self):
+        return self.joueurs[self.tour_de_jeu % 2]
+
+
+batons_initial = int(raw_input("Avec combien de batons voulez vous jouer ? "))
+game = NimGame(batons_initial, ["Henri", "Étienne"]) 
+print "Vous jouez avec %d batons !" % game.batons
+
+while game.encore_des_batons():
+    batons_retirer = int(raw_input("%s combien voulez vous retirez de batons ? " % game.joueur_courant()))
+    game.retire(batons_retirer)
+    print "il reste %d batons" % game.batons
