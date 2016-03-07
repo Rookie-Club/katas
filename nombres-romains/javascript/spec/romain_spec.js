a66842a5e86c0b83eb7f77c1b0e44f4d58444e91
Simon <sims38930@gmail.com>
2016-03-07 16:01:06 +0100

    on avance sur le convertisseur
    nous sommes capable de faire des aditions, et on test la boucle ET la recursivité

--- nombres-romains/javascript/spec/romain_spec.js
+++ nombres-romains/javascript/spec/romain_spec.js
@@ -1,11 +1,30 @@
 describe("romain", function (){
+    assertRomain = function (nombre_romain, entier) {
+        if (nombre_romain === undefined) {
+            expect(romain()).toEqual(entier);
+            expect(romain_recursif()).toEqual(entier);
+        } else {
+            expect(romain(nombre_romain)).toEqual(entier);
+            expect(romain_recursif(nombre_romain)).toEqual(entier);
+        }
+    }
+
     it("renvoi 0", function (){
-        expect(romain()).toEqual(0)
+        assertRomain(undefined, 0);
     });
     it("renvoi 1 pour I", function (){
-        expect(romain("I")).toEqual(1)
+        assertRomain("I", 1);
     });
     it("renvoi 5 pour V", function (){
-        expect(romain("V")).toEqual(5)
+        assertRomain("V", 5);
+    });
+    it("renvoi 10 pour X", function (){
+        assertRomain("X", 10);
+    });
+    it("renvoi 2 pour II", function (){
+        assertRomain("II", 2);
+    });
+    it("renvoi 7 pour VII", function (){
+        assertRomain("VII", 7);
     });
 });
--- nombres-romains/javascript/src/romain.js
+++ nombres-romains/javascript/src/romain.js
@@ -1,4 +1,20 @@
 romain = function (){
-    var dico = {undefined:0, "I":1, "V":5};
-    return dico[arguments[0]];
+    if (!arguments.length || arguments[0] === "") { return 0 }
+
+    var nombre_romain = arguments[0].split(""); // ["I"]
+    var dico = {"I":1, "V":5, "X":10};
+
+    var result = 0;
+    for(var i = 0; i < nombre_romain.length; i++) {
+        result += dico[nombre_romain[i]]
+    }
+    return result;
+};
+
+romain_recursif = function (){
+    if (!arguments.length || arguments[0] === "") { return 0 }
+    var nombre_romain = arguments[0];
+    var dico = {"I":1, "V":5, "X":10};
+    romain_array = nombre_romain.split("").reverse(); //=> ["I", "I"]
+    return dico[romain_array.pop(0)] + romain(romain_array.join(""))
 };
