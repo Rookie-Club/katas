79684f67fbb9d0cb15b68eb7bd5515fc1c1efb23
Simon <sims38930@gmail.com>
2016-03-09 11:40:20 +0100

    ajoute des tests sur les chiffres suivants

--- nombres-romains/python/romains_test.py
+++ nombres-romains/python/romains_test.py
@@ -1,11 +1,19 @@
 import unittest
 
 def convertis(nombre):
+    if nombre == 2:
+        return "II"
     return "I"
 
 class RomainsTest(unittest.TestCase):
     def test_1_donne_I(self):
         self.assertEqual("I", convertis(1))
+
+    def test_2_donne_II(self):
+        self.assertEqual("II", convertis(2))
+
+    def test_3_donne_III(self):
+        self.assertEqual("III", convertis(3))
 
 if __name__ == "__main__":
     unittest.main()
