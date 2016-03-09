760f14a685aefc393bdfe4e20be682109adcfd9a
Simon <sims38930@gmail.com>
2016-03-09 11:10:47 +0100

    convertis 1 en I

--- nombres-romains/python/romains_test.py
+++ nombres-romains/python/romains_test.py
@@ -1,8 +1,11 @@
 import unittest
 
+def convertis(nombre):
+    return "I"
+
 class RomainsTest(unittest.TestCase):
-    def test_truth(self):
-        self.assertEqual(True, True)
+    def test_1_donne_I(self):
+        self.assertEqual("I", convertis(1))
 
 if __name__ == "__main__":
     unittest.main()
