2342345a12002788bbae6aedaebea2401823226f
HafidTraikzi <hafid.traikzi@gmail.com>
2016-03-22 15:29:18 +0100

    La home page est un template

--- nim/python/nim_server.py
+++ nim/python/nim_server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "2x+cool"
+    return render_template("nim.html")
 
 @app.route("/jouer")
 def jouer():
