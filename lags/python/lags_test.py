9770b47ba940c84151a7da152c995d827dd25cfb
HafidTraikzi <hafid.traikzi@gmail.com>
2016-03-23 16:16:43 +0100

    on traite 3 demandes compatibles

--- lags/python/lags_test.py
+++ lags/python/lags_test.py
@@ -4,7 +4,10 @@ def gain_max(demandes = []):
     if not demandes: return 0
     if len(demandes) == 1:
         return prix(demandes[0])
-    return prix(demandes[0]) + prix(demandes[1])
+    if len(demandes) == 2:
+        return prix(demandes[0]) + prix(demandes[1])
+    if len(demandes) == 3:
+        return prix(demandes[0]) + prix(demandes[1]) + prix(demandes[2])
 
 def prix(demande):
     return demande[2]
@@ -18,5 +21,8 @@ class LagsTest(unittest.TestCase):
 
     def test_2_avec_2_demandes_compatibles(self):
         self.assertEqual(2, gain_max([[1,1,1], [2,1,1]]))
+
+    def test_5_avec_3_demandes_compatibles(self):
+        self.assertEqual(5, gain_max([[1,1,1], [2,1,1], [4,1,3]]))
 
 unittest.main()
