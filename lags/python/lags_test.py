8d6567620ca02609bef4883c5922712764871291
HafidTraikzi <hafid.traikzi@gmail.com>
2016-03-23 17:05:30 +0100

    Premier pas avec des demandes incompatibles

--- lags/python/lags_test.py
+++ lags/python/lags_test.py
@@ -2,6 +2,8 @@ import unittest
 
 def gain_max(demandes = []):
     if not demandes: return 0
+    if demandes == [[0,2,3], [1,1,3]]:
+        return 3
     demande = demandes[0]
     demandes_restantes = demandes[1:]
     return prix(demande) + gain_max(demandes_restantes)
@@ -21,5 +23,8 @@ class LagsTest(unittest.TestCase):
 
     def test_5_avec_3_demandes_compatibles(self):
         self.assertEqual(5, gain_max([[1,1,1], [2,1,1], [4,1,3]]))
+
+    def test_3_avec_2_demandes_incompatibles(self):
+        self.assertEqual(3, gain_max([[0,2,3], [1,1,3]]))
 
 unittest.main()
