e0c9af2820e23faebc3858ecfe9bc483f2b52b61
HafidTraikzi <hafid.traikzi@gmail.com>
2016-03-23 15:32:36 +0100

    le prix pour une demande

--- lags/python/lags_test.py
+++ lags/python/lags_test.py
@@ -1,10 +1,14 @@
 import unittest
 
-def gain_max():
-    return 0
+def gain_max(demandes = []):
+    if not demandes: return 0
+    return demandes[0][2]
 
 class LagsTest(unittest.TestCase):
     def test_0_sans_demande(self):
         self.assertEqual(0, gain_max())
+
+    def test_1_avec_1_demande(self):
+        self.assertEqual(1, gain_max([[1,0,1]]))
 
 unittest.main()
