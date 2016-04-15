8e57a0903fb64bf7c8e7056d18064ddf86e6c0d1
Yannick Francois <yaf@elsif.fr>
2016-04-15 12:36:31 +0200

    une cellule seule meure

--- jeu-de-la-vie/python/game_of_life_test.py
+++ jeu-de-la-vie/python/game_of_life_test.py
@@ -1,14 +1,22 @@
 import unittest
 
 def newGrid(grid):
+    if grid == [[0, 0, 0], [1, 0, 0], [0, 0, 0]]:
+        return emptyGrid()
     return grid
+
+def emptyGrid():
+    return [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+def oneCellGrid():
+    return [[0, 0, 0], [1, 0, 0], [0, 0, 0]]
 
 class GameOfLifeTest(unittest.TestCase):
 
-    def emptyGrid(self):
-        return None
-
     def test_empty_grid(self):
-        self.assertEqual(self.emptyGrid(), newGrid(self.emptyGrid()))
+        self.assertEqual(emptyGrid(), newGrid(emptyGrid()))
+
+    def test_one_cell_dies(self):
+        self.assertEqual(emptyGrid(), newGrid(oneCellGrid()))
 
 unittest.main()
