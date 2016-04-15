4625c881f13c75515ec343f020901f27f886e19a
Yannick Francois <yaf@elsif.fr>
2016-04-15 12:55:18 +0200

    on commence à parcourir la grille

--- jeu-de-la-vie/python/game_of_life_test.py
+++ jeu-de-la-vie/python/game_of_life_test.py
@@ -1,9 +1,16 @@
 import unittest
 
 def newGrid(grid):
-    if grid == [[0, 0, 0], [1, 0, 0], [0, 0, 0]]:
-        return emptyGrid()
-    return grid
+    newGrid = emptyGrid()
+    for lineIndex, line in enumerate(grid):
+        for cellIndex, cell in enumerate(line):
+
+            if cell == 1:
+                newGrid[lineIndex][cellIndex] = 0
+            else:
+                newGrid[lineIndex][cellIndex] = cell
+
+    return newGrid
 
 def emptyGrid():
     return [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
