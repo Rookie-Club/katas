ed2062d537930e1810a5dd281be4a37c1bcd1973
HafidTraikzi <hafid.traikzi@gmail.com>
2016-04-29 16:14:43 +0200

    Introduis notion de direction; et (finalement) remets des fonctions histoire que ça marche

--- langton/javascript/session_20160429/spec/FourmiSpec.js
+++ langton/javascript/session_20160429/spec/FourmiSpec.js
@@ -1,13 +1,13 @@
 describe("La Fourmi de Langton", function () {
     it("est au milieu", function () {
         var monde = nouveauMonde(20);
-        expect(monde.positionFourmi).toEqual({x: 10, y: 10});
+        expect(monde.positionFourmi()).toEqual({x: 10, y: 10});
     });
 
     it("au debut se deplace au nord", function () {
         var monde = nouveauMonde(20);
         monde.bougeFourmi();
-        expect(monde.positionFourmi).toEqual({x: 10, y: 9});
+        expect(monde.positionFourmi()).toEqual({x: 10, y: 9});
     });
 
     it("color la case", function () {
@@ -15,5 +15,12 @@ describe("La Fourmi de Langton", function () {
         expect(monde.couleurCase(10, 9)).toEqual("blanc");
         monde.bougeFourmi();
         expect(monde.couleurCase(10, 9)).toEqual("noir");
+    });
+
+    it("tourne a droit quand arrive sur case blanche", function () {
+        var monde = nouveauMonde(20);
+        expect(monde.directionFourmi()).toEqual({dx: 0, dy: -1});
+        monde.bougeFourmi();
+        expect(monde.directionFourmi()).toEqual({dx: 1, dy: 0});
     });
 });
--- langton/javascript/session_20160429/src/Fourmi.js
+++ langton/javascript/session_20160429/src/Fourmi.js
@@ -1,22 +1,30 @@
 window.nouveauMonde = function (dimension) {
-    var positionFourmi = {x: dimension/2, y: dimension/2};
+    var position = {x: dimension/2, y: dimension/2};
     var couleur = "blanc";
+    var direction = {dx: 0, dy: -1};
 
     var couleurCase = function(x, y) {
         return couleur;
     };
 
+    var positionFourmi = function () { return position; }
+    var directionFourmi = function () { return direction; }
+
     var bougeFourmi = function () {
         var decalage = {dx: 0, dy: -1};
-        positionFourmi.x += decalage.dx;
-        positionFourmi.y += decalage.dy;
+        position.x += decalage.dx;
+        position.y += decalage.dy;
+
+        direction = {dx: 1, dy: 0};
 
         couleur = "noir";
     };
 
+
     return {
         positionFourmi: positionFourmi,
         bougeFourmi: bougeFourmi,
+        directionFourmi: directionFourmi,
         couleurCase: couleurCase
     };
 };
