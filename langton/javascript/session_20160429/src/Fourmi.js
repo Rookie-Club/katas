fd723120fb5016bddd35dd3596fb3b790cc78a09
HafidTraikzi <hafid.traikzi@gmail.com>
2016-04-29 14:30:44 +0200

    Remaniement : - generalise le centrage à la dimension du monde - introduit notion de decalage

--- langton/javascript/session_20160429/src/Fourmi.js
+++ langton/javascript/session_20160429/src/Fourmi.js
@@ -1,12 +1,16 @@
-window.nouveauMonde = function () {
-    var position = {x: 10, y: 10};
+window.nouveauMonde = function (dimension) {
+    var position = {x: dimension/2, y: dimension/2};
 
     var positionFourmi = function () {
         return position;
     };
 
     var bougeFourmi = function () {
-        return position = {x: 10, y: 9};
+        var decalage = {dx: 0, dy: -1};
+        position.x += decalage.dx;
+        position.y += decalage.dy;
+
+        return position
     };
 
     return {
