2eb7243b0696f8518cffffc841699c3e997ba1de
HafidTraikzi <hafid.traikzi@gmail.com>
2016-04-29 15:09:40 +0200

    Introduis notion de couleur

--- langton/javascript/session_20160429/spec/FourmiSpec.js
+++ langton/javascript/session_20160429/spec/FourmiSpec.js
@@ -1,12 +1,19 @@
 describe("La Fourmi de Langton", function () {
     it("est au milieu", function () {
         var monde = nouveauMonde(20);
-        expect(monde.positionFourmi()).toEqual({x: 10, y: 10});
+        expect(monde.positionFourmi).toEqual({x: 10, y: 10});
     });
 
     it("au debut se deplace au nord", function () {
         var monde = nouveauMonde(20);
         monde.bougeFourmi();
-        expect(monde.positionFourmi()).toEqual({x: 10, y: 9});
+        expect(monde.positionFourmi).toEqual({x: 10, y: 9});
+    });
+
+    it("color la case", function () {
+        var monde = nouveauMonde(20);
+        expect(monde.couleurCase(10, 9)).toEqual("blanc");
+        monde.bougeFourmi();
+        expect(monde.couleurCase(10, 9)).toEqual("noir");
     });
 });
--- langton/javascript/session_20160429/src/Fourmi.js
+++ langton/javascript/session_20160429/src/Fourmi.js
@@ -1,20 +1,22 @@
 window.nouveauMonde = function (dimension) {
-    var position = {x: dimension/2, y: dimension/2};
+    var positionFourmi = {x: dimension/2, y: dimension/2};
+    var couleur = "blanc";
 
-    var positionFourmi = function () {
-        return position;
+    var couleurCase = function(x, y) {
+        return couleur;
     };
 
     var bougeFourmi = function () {
         var decalage = {dx: 0, dy: -1};
-        position.x += decalage.dx;
-        position.y += decalage.dy;
+        positionFourmi.x += decalage.dx;
+        positionFourmi.y += decalage.dy;
 
-        return position
+        couleur = "noir";
     };
 
     return {
         positionFourmi: positionFourmi,
-        bougeFourmi: bougeFourmi
+        bougeFourmi: bougeFourmi,
+        couleurCase: couleurCase
     };
 };
