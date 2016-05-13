3c802242b1075368aa975b3250972f43447dc807
Yannick Francois <yaf@elsif.fr>
2016-05-13 17:06:20 +0200

    changement d'API pour la position

--- langton/javascript/spec/FourmiSpec.js
+++ langton/javascript/spec/FourmiSpec.js
@@ -11,12 +11,31 @@ describe("La Fourmi de Langton", function () {
 
   it("est orienté au nord", function () {
     var monde = creeUnAutreMonde(20);
-    expect(monde.orientationFourmi).toEqual({x: 9, y: 10});
+    expect(monde.orientationFourmi).toEqual("N");
   });
 
   it("est orienté au nord d'un petit monde", function () {
     var monde = creeUnAutreMonde(10);
-    expect(monde.orientationFourmi).toEqual({x: 4, y: 5});
+    expect(monde.orientationFourmi).toEqual("N");
+  });
+
+  it("tourne a gauche", function () {
+    var monde = creeUnAutreMonde(20);
+    monde.faitTournerLaFourmiAGauche();
+    expect(monde.orientationFourmi).toEqual("O");
+  });
+
+  it("tourne a gauche toujours", function () {
+    var monde = creeUnAutreMonde(20);
+    monde.faitTournerLaFourmiAGauche();
+    monde.faitTournerLaFourmiAGauche();
+    expect(monde.orientationFourmi).toEqual("S");
+  });
+
+  it("avance d'une case", function () {
+    var monde = creeUnAutreMonde(20);
+    monde.faitAvancerLaFourmi();
+    expect(monde.positionFourmi).toEqual({x: 10, y: 9});
   });
 
 });
--- langton/javascript/src/Fourmi.js
+++ langton/javascript/src/Fourmi.js
@@ -1,12 +1,33 @@
 window.creeUnAutreMonde = function (dimension) {
 
   var milieu = dimension / 2;
+  var orientations = [{label: "N", modifications: {x: 0, y: -1}},
+    {label: "E", modifications: {x: 1, y: 0}},
+    {label: "S", modifications: {x: 0, y: 1}},
+    {label: "O", modifications: {x: -1, y: 0}}];
 
-  var point_initial = {x: milieu, y: milieu};
-  var au_nord = {x: milieu - 1, y: milieu};
+  var pointInitial = {x: milieu, y: milieu};
+  var orientationInitiale = 0;
 
-  return {
-    positionFourmi: point_initial,
-    orientationFourmi: au_nord
+  var nouveauMonde = {
+    positionFourmi: pointInitial,
+    _orientationFourmi: orientationInitiale,
+    orientationFourmi: orientations[orientationInitiale].label,
+    faitTournerLaFourmiAGauche: function () {
+      var nouvelleOrientation = this._orientationFourmi - 1;
+      if (nouvelleOrientation < 0) {
+        nouvelleOrientation = 3;
+      }
+      this._orientationFourmi = nouvelleOrientation;
+      this.orientationFourmi = orientations[nouvelleOrientation].label;
+    },
+    faitAvancerLaFourmi: function () {
+      var nouvellePosition;
+      var modifications = orientations[this._orientationFourmi].modifications;
+      nouvellePosition = {x: this.positionFourmi.x + modifications.x, y: this.positionFourmi.y + modifications.y};
+      this.positionFourmi = nouvellePosition;
+    }
   };
+
+  return nouveauMonde;
 }
