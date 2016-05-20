d2121ee076a9f1866dc72e851e5ddd2ac5fb99bb
HafidTraikzi <hafid.traikzi@gmail.com>
2016-05-20 11:26:30 +0200

    Kata fourmiLangton en 45min

--- langton/javascript/spec/FourmiSpec.js
+++ langton/javascript/spec/FourmiSpec.js
@@ -1,36 +1,10 @@
-describe("La Fourmi de Langton", function () {
-  it("est au centre", function () {
-    var monde = creeUnAutreMonde(20);
-    expect(monde.positionFourmi).toEqual({x: 10, y: 10});
-  });
-
-  it("est au centre d'un petit monde", function () {
-    var monde = creeUnAutreMonde(10);
-    expect(monde.positionFourmi).toEqual({x: 5, y: 5});
-  });
-
-  it("prochain mouvement en position initiale", function () {
-    var monde = creeUnAutreMonde(20);
-    expect(monde.prochainMouvement()).toEqual({x: 0, y: -1});
-  });
-
-  it("prochain mouvement apres une rotation à gauche", function () {
-    var monde = creeUnAutreMonde(20);
-    monde.faitTournerLaFourmiAGauche();
-    expect(monde.prochainMouvement()).toEqual({x: -1, y: 0});
-  });
-
-  it("prochain mouvement apres deux rotation à gauche", function () {
-    var monde = creeUnAutreMonde(20);
-    monde.faitTournerLaFourmiAGauche();
-    monde.faitTournerLaFourmiAGauche();
-    expect(monde.prochainMouvement()).toEqual({x: 0, y: 1});
-  });
-
-  it("avance d'une case", function () {
-    var monde = creeUnAutreMonde(20);
-    monde.faitAvancerLaFourmi();
-    expect(monde.positionFourmi).toEqual({x: 10, y: 9});
-  });
-
+describe("La fourmi de Langton", function () {
+    it("est au centre", function () {
+        var monde = autreMonde(20);
+        expect(monde.positionFourmi()).toEqual({x: 10, y: 10});
+    });
+    it("orientée vers le nord", function () {
+        var monde = autreMonde(20);
+        expect(monde.orientationFourmi()).toEqual({x: 10, y: 9});
+    });
 });
--- langton/javascript/src/Fourmi.js
+++ langton/javascript/src/Fourmi.js
@@ -1,32 +1,14 @@
-window.creeUnAutreMonde = function (dimension) {
+window.autreMonde = function () {
+    var positionFourmi = function () {
+        return {x: 10, y: 10};
+    };
 
-  var milieu = dimension / 2;
-  var orientations = [{x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}];
+    var orientationFourmi = function () {
+        return {x: 10, y: 9};
+    };
 
-  var pointInitial = {x: milieu, y: milieu};
-  var orientationInitiale = 0;
-
-  var nouveauMonde = {
-    positionFourmi: pointInitial,
-    _orientationFourmi: orientationInitiale,
-
-    faitTournerLaFourmiAGauche: function () {
-      var nouvelleOrientation = this._orientationFourmi - 1;
-      if (nouvelleOrientation < 0) {
-        nouvelleOrientation = orientations.length - 1;
-      }
-      this._orientationFourmi = nouvelleOrientation;
-    },
-    faitAvancerLaFourmi: function () {
-      var nouvellePosition;
-      var modifications = this.prochainMouvement();
-      nouvellePosition = {x: this.positionFourmi.x + modifications.x, y: this.positionFourmi.y + modifications.y};
-      this.positionFourmi = nouvellePosition;
-    },
-    prochainMouvement: function () {
-      return orientations[this._orientationFourmi];
-    }
-  };
-
-  return nouveauMonde;
-}
+    return {
+        positionFourmi: positionFourmi,
+        orientationFourmi: orientationFourmi
+    };
+};
