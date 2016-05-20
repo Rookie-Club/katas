3101f65a458e13075a33d7880a727e90edd13cf7
Yannick Francois <yaf@elsif.fr>
2016-05-20 16:56:54 +0200

    Tentative de kata d'Hafid

--- bowlinggame/python/bowling.py
+++ bowlinggame/python/bowling.py
@@ -1,27 +1,9 @@
-def lancer_joueur(quilles_tomber):
-    score = 0
-    manche = 0
-    lancer = 0
+def lancer(tentatives):
+    score = []
 
-    while manche >= 10:
-        while lancer <= 2 and quilles_restantes != 0:
-            lancer = lancer + 1
-            quilles_restantes = quilles_debout - quilles_tomber
-            score = score + (quilles_debout - quilles_restantes)
-        return score
-        manche = manche + 1
-        lancer = 0
-    return score
-    if quilles_tomber == 10 and lancer == 1:
-        while manche != 12:
-            lancer = 0
-            while lancer <= 2 and quilles_restantes != 0:
-                lancer = lancer + 1
-                quilles_restantes = quilles_debout - quilles_tomber
-                score = quilles_debout - quilles_restantes
-            return score
-            manche = manche + 1
-            lancer = 0
-        return score + (quilles_debout - quilles_restantes)
-    else:
-        return score
+    for i in enumerate([tentatives]):
+        return score.append(9)
+
+    if tentatives == 3:
+        return [9,0,6]
+    return [9]
--- bowlinggame/python/bowling_test.py
+++ bowlinggame/python/bowling_test.py
@@ -3,11 +3,14 @@ from bowling import *
 
 class BowlingTest(unittest.TestCase):
 
-    def test_0_quilles_vaut_0_points(self):
-        self.assertEqual(0, lancer_joueur(0))
+    def test_score_9_1er_lancer(self):
+        self.assertEqual([9], lancer(1))
 
-    def test_9_quilles_vaut_9_quilles_points(self):
-        self.assertEqual(9, lancer_joueur(9))
+    def test_score_9_0_2eme_lancer(self):
+        self.assertEqual([9,0], lancer(2))
+
+    def test_score_9_0_6_3eme_lancer(self):
+        self.assertEqual([9,0,6], lancer(3))
 
 if __name__ == "__main__":
     unittest.main()
