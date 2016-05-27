ea959b5e2f1e33b4ead07bc8c11f53abcc044cc9
Yannick Francois <yaf@elsif.fr>
2016-05-27 17:02:46 +0200

    On commence à compter les points

--- bowlinggame/python/bowlingTest.py
+++ bowlinggame/python/bowlingTest.py
@@ -1,8 +1,16 @@
 import unittest
 
+def score(quilles):
+    if quilles == [1]:
+        return 1
+    return 0
+
 class BowlingTest(unittest.TestCase):
 
     def test_0_quilles_vaut_0(self):
         self.assertEqual(0, score([]))
+
+    def test_1_quilles_vaut_1(self):
+        self.assertEqual(1, score([1]))
 
 unittest.main()
