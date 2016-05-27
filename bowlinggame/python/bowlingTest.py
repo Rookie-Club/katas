d300b1d3606a2dc1eea949fde29ef99b8a126dac
Yannick Francois <yaf@elsif.fr>
2016-05-27 17:15:53 +0200

    on lance deux fois la boule

--- bowlinggame/python/bowlingTest.py
+++ bowlinggame/python/bowlingTest.py
@@ -1,9 +1,11 @@
 import unittest
 
 def score(quilles):
-    if quilles == [1]:
-        return 1
-    return 0
+    if len(quilles) == 0:
+        return 0
+    if quilles == [2, 1]:
+        return 3
+    return quilles[0]
 
 class BowlingTest(unittest.TestCase):
 
@@ -12,5 +14,11 @@ class BowlingTest(unittest.TestCase):
 
     def test_1_quilles_vaut_1(self):
         self.assertEqual(1, score([1]))
+
+    def test_3_quilles_vaut_3(self):
+        self.assertEqual(3, score([3]))
+
+    def test_3_quilles_en_2_fois_vaut_3(self):
+        self.assertEqual(2 + 1, score([2, 1]))
 
 unittest.main()
