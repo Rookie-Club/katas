04100d82c16ebabc8387c8a67c2132183dcaf7b6
Yannick Francois <yaf@elsif.fr>
2016-05-27 16:13:37 +0200

    Le Kata de Hafid

---
+++ nombres-romains/python/romanToNumber.py
@@ -0,0 +1,7 @@
+def convert(letter):
+    if letter == "I":
+        return 1
+    if letter == "I" + "I":
+        return 1 + 1
+    if letter == "I" + "I" + "I":
+        return 1 + 1 + 1
---
+++ nombres-romains/python/romanToNumber_test.py
@@ -0,0 +1,15 @@
+import unittest
+from romanToNumber import *
+
+class romanToNumberTest(unittest.TestCase):
+    def test_I_donne_1(self):
+        self.assertEqual(1, convert("I"))
+
+    def test_II_donne_2(self):
+        self.assertEqual(2, convert("II"))
+
+    def test_III_donne_3(self):
+        self.assertEqual(3, convert("III"))
+
+if __name__ == "__main__":
+    unittest.main()
