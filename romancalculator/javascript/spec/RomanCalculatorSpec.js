10fa862f393bf235bf3512bab412107ace03d5f9
Yannick Francois <yaf@elsif.fr>
2016-06-10 15:23:04 +0200

    Premier pas pour l'addition

--- romancalculator/javascript/spec/RomanCalculatorSpec.js
+++ romancalculator/javascript/spec/RomanCalculatorSpec.js
@@ -1,1 +1,8 @@
 
+// Recette "XIV" + "LX" = "LXXIV
+
+describe("RomanCalculator", function () {
+  it("return II when I + I", function () {
+    expect(RomanCalculator.plus("I", "I")).toBe("I" + "I");
+  });
+});
---
+++ romancalculator/javascript/src/RomanCalculator.js
@@ -0,0 +1,5 @@
+var RomanCalculator = {
+  plus: function () {
+    return "II";
+  }
+}
