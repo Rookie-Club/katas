06d1e901d784a6a2fe88f618285e1352090788ec
Yannick Francois <yaf@elsif.fr>
2016-06-10 16:54:49 +0200

    On fini de corriger le code pour correspondre à la nouvelle API

--- romancalculator/javascript/src/romanCalculator.js
+++ romancalculator/javascript/src/romanCalculator.js
@@ -1,7 +1,15 @@
 var RomanCalculator = function () {
   return {
+    terms: [],
     add: function () {
-      return "";
+      var result = "";
+      for (var i = 0; i < this.terms.length; i++) {
+        result += this.terms[i];
+      }
+      return result;
+    },
+    enter: function (term) {
+      this.terms.push(term);
     }
   }
 }
