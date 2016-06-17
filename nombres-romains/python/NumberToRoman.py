62189615bfcfa3a4bff187e4121153b6e5760d85
Yannick Francois <yaf@elsif.fr>
2016-06-17 10:45:43 +0200

    Ajout du cas 1 donne I

--- nombres-romains/python/NumberToRoman.py
+++ nombres-romains/python/NumberToRoman.py
@@ -1,2 +1,4 @@
 def convert(numbers):
+    if numbers == 1:
+        return "I"
     return ""
--- nombres-romains/python/NumberToRomanTest.py
+++ nombres-romains/python/NumberToRomanTest.py
@@ -5,5 +5,8 @@ class NumberToRomanTest(unittest.TestCase):
     def test_0_donne_vide(self):
         self.assertEqual("", convert(0))
 
+    def test_1_donne_I(self):
+        self.assertEqual("I", convert(1))
+
 if __name__ == "__main__":
     unittest.main()
