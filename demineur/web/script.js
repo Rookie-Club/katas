6d58c945857b96e313bcf05cd2e77758e50aa029
Yannick Francois <yaf@elsif.fr>
2016-06-17 16:07:20 +0200

    on est capable de redémarrer la partie

--- demineur/web/script.js
+++ demineur/web/script.js
@@ -2,9 +2,15 @@ window.onload = function () {
   console.log("loaded");
   var tds = document.getElementsByTagName('td');
   console.log(tds);
-  for(var i in tds) {
+  for (var i in tds) {
     tds[i].onclick = function () {
       this.classList.add("boom");
+    };
+  }
+
+  starter.onclick = function () {
+    for (var i in tds) {
+      tds[i].className = "base";
     }
   };
-}
+};
