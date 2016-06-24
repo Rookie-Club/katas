bce841e8f9521bc4800844983b9edbad465ae82c
HafidTraikzi <hafid.traikzi@gmail.com>
2016-06-24 17:18:24 +0200

    Tentative pour indiquer les infos autour des mines

--- demineur/web/script.js
+++ demineur/web/script.js
@@ -15,6 +15,12 @@ var revealCase = function () {
   }
 };
 
+var clueCase = function () {
+  //this.classList.add("clue_plus_1");
+  //clue = this.classList.contains("clue_plus_1");
+  //if (this.classList.contains("clue_plus_1")) { return clue.innerHTML = "1"; }
+}
+
 var random_case = function () {
   return Math.floor(Math.random() * 64) + 1;
 }
@@ -36,6 +42,7 @@ window.onload = function () {
   var tds = document.getElementsByTagName("td");
   var number_of_mines = 10;
   var cases_with_mine = makeCasesWithMineList(number_of_mines);
+  var clue_plus_1 = clueCase(number_of_mines);
 
   for (var i = 0; i < tds.length; i++) {
 
@@ -43,8 +50,13 @@ window.onload = function () {
       tds[i].setAttribute('mine', true);
       number_of_mines -= 1;
     }
+    
+    if (number_of_mines > 0 && cases_with_mine.includes(i+1 && i-1) && !cases_with_mine.includes(i)) {
+      console.log("Clue");
+      tds[i].setAttribute('clue_plus_1', true);
+    }
 
     tds[i].onclick = revealCase;
+    tds[i].onclick = clueCase;
   }
-
 };
