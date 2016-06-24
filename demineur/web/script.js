02c5c044592ee36069c2268e04100b9fe96a528d
Yannick Francois <yaf@elsif.fr>
2016-06-24 12:37:11 +0200

    On avance sur un démineur tous ensemble ce matin

--- demineur/web/script.js
+++ demineur/web/script.js
@@ -1,37 +1,19 @@
-const addEventOnCell = function () {
-  tds = document.getElementsByTagName('td');
-  for (var i in tds) {
-    tds[i].onclick = function () {
-      this.classList.add("boom");
-    };
+var bombOnCase = function () {
+  return (new Date() % 3) == 0;
+};
+
+var revealCase = function () {
+  if (this.classList.contains("revealedCase")) { return; }
+
+  this.classList.add("revealedCase");
+  if(bombOnCase()) {
+    this.classList.add("bomb");
   }
 };
 
-const buildCells = function (quantity, row) {
-  var cell;
-  for (var i = 0; i < quantity; i++) {
-    cell = document.createElement("td");
-    cell.className = "base";
-    row.appendChild(cell);
-  };
-};
-
-const buildRows = function (quantity, table) {
-  var row;
-  for (var i = 0; i < quantity; i++) {
-    row = document.createElement("tr");
-    buildCells(quantity, row);
-    table.appendChild(row);
-  };
-};
-
 window.onload = function () {
-
-  starter.onclick = function () {
-    grid.innerHTML = "";
-    var table = document.createElement("table");
-    buildRows(3, table);
-    grid.appendChild(table);
-    addEventOnCell();
-  };
+  var tds = document.getElementsByTagName("td");
+  for (var i = 0; i < tds.length; i++) {
+    tds[i].onclick = revealCase;
+  }
 };
