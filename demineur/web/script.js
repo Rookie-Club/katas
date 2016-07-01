2410f3f332483e838beb4a004bb89ae8eec7bd8b
yohannbos <bosco.yohann@gmail.com>
2016-07-01 11:59:31 +0200

    a mis le demineur sur une seule table
    avec classe et elegance
    
    pour tester les commits sur plusieurs lignes

--- demineur/web/script.js
+++ demineur/web/script.js
@@ -1,47 +1,45 @@
 window.onload = function () {
 
-    var hiddenBomb = function () {
-      var tds = document.getElementsByTagName("td");
+  var hiddenBomb = function () {
+    var tds = document.getElementsByTagName("td");
 
-      for (var i = 0; i < tds.length; i++) {
-        tds[i].onclick = function () {
-          return this.style.backgroundColor = "red";
-        }
+    for (var i = 0; i < tds.length; i++) {
+      tds[i].onclick = function () {
+        return this.style.backgroundColor = "red";
       }
     }
+  }
 
-    var generateGrid = function (numberOfTable, numberOfTd) {
-      for (var i = 0; i < numberOfTable; i++) {
-        var table = document.createElement("table");
-        document.body.appendChild(table);
+  var generateGrid = function (gridSize) {
+    var table = document.createElement("table");
+    document.body.appendChild(table);
 
-        for (var j = 0; j < 1; j++) {
-          var tr = document.createElement("tr");
-          table.appendChild(tr);
+    for (var i = 0; i < gridSize; i++) {
+      var tr = document.createElement("tr");
+      table.appendChild(tr);
 
-          for (var k = 0; k < numberOfTd; k++) {
-            var td = document.createElement("td");
-            tr.appendChild(td.cloneNode(true), document.body.lastChild);
-          }
-        }
+      for (var k = 0; k < gridSize; k++) {
+        var td = document.createElement("td");
+        tr.appendChild(td);
       }
     }
+  }
 
-    var createBomb = function () {
-      var newCase = [];
-      var numberOfMine = 10;
-      var random = randomBomb();
-      for (var i = 0; i < numberOfMine; i++) {
-        newCase.push(random);
-      }
+  var createBomb = function () {
+    var newCase = [];
+    var numberOfMine = 10;
+    var random = randomBomb();
+    for (var i = 0; i < numberOfMine; i++) {
+      newCase.push(random);
     }
+  }
 
-    var randomBomb = function () {
-      return Math.floor(Math.random() * 64);
-    }
+  var randomBomb = function () {
+    return Math.floor(Math.random() * 64);
+  }
 
-    generateGrid(8, 8);
-    hiddenBomb();
-    createBomb();
-    randomBomb();
+  generateGrid(8);
+  hiddenBomb();
+  createBomb();
+  randomBomb();
 }
