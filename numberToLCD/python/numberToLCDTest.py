4b4e793737e6db4fe94d1409f88014aeb5e28b0c
Yannick Francois <yaf@elsif.fr>
2016-07-01 15:44:55 +0200

    ajout du cas pour un

--- numberToLCD/python/numberToLCDTest.py
+++ numberToLCD/python/numberToLCDTest.py
@@ -1,11 +1,16 @@
 import unittest
 
 def numbertoLCD():
-    return ''
+    return ['    ', '    ', '    ', '    ', '    ', '    ', '    ']
 
 class NumberToLCDTest(unittest.TestCase):
 
     def test_vide_n_affiche_rien(self):
-        self.assertEqual('', numbertoLCD())
+        expected = ['    ', '    ', '    ', '    ', '    ', '    ', '    ']
+        self.assertEqual(expected, numbertoLCD())
+
+    def test_1_affiche_un(self):
+        expected = ['    ', '   |', '   |', '    ', '   |', '   |', '    ']
+        self.assertEqual(expected, numbertoLCD(1))
 
 unittest.main()
