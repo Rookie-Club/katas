50dfd788f11ed3725e54feaff7c63a9d373885a5
HafidTraikzi <hafid.traikzi@gmail.com>
2016-07-08 10:31:37 +0200

    Ajout numberToLCD par Hafid.

--- numberToLCD/python/numberToLCD.py
+++ numberToLCD/python/numberToLCD.py
@@ -1,25 +1,28 @@
-
 def numberToLCD(number = None):
-    dico = {None:['    ', '    ', '    ', '    ', '    ', '    ', '    '],
+    dico = {None :['    ', '    ', '    ', '    ', '    ', '    ', '    '],
             0: [' -- ', '|  |', '|  |', '    ', '|  |', '|  |', ' -- '],
-            1: ['    ', '   |', '   |', '    ', '   |', '   |', '    ']}
+            1: ['    ', '   |', '   |', '    ', '   |', '   |', '    '],
+            2: [' -- ', '   |', '   |', ' -- ', '|   ', '|   ', ' -- '],
+            3: ['    ', '   |', '   |', '    ', '   |', '   |', '    '],
+            4: ['    ', '   |', '   |', '    ', '   |', '   |', '    '],
+            5: ['    ', '   |', '   |', '    ', '   |', '   |', '    '],
+            6: ['    ', '   |', '   |', '    ', '   |', '   |', '    '],
+            7: ['    ', '   |', '   |', '    ', '   |', '   |', '    '],
+            8: ['    ', '   |', '   |', '    ', '   |', '   |', '    '],
+            9: ['    ', '   |', '   |', '    ', '   |', '   |', '    '],
+            10: [' -- ', '|  |', '|  |', '    ', '|  |', '|  |', ' -- ']}
 
-    espace = [' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    if (number == 10):
-        dizaine = number // 10
-        unite = number % 10
-        lcd_array = [
-                dico[1][0] + espace[0] + dico[0][0],
-                dico[1][1] + espace[1] + dico[0][1],
-                dico[1][2] + espace[2] + dico[0][2],
-                dico[1][3] + espace[3] + dico[0][3],
-                dico[1][4] + espace[4] + dico[0][4],
-                dico[1][5] + espace[5] + dico[0][5],
-                dico[1][6] + espace[6] + dico[0][6]
-                ]
+    space = [' ', ' ', ' ', ' ', ' ', ' ', ' ']
+
+    lcd_array = []
+    exclude = set({None :['    ', '    ', '    ', '    ', '    ', '    ', '    ']})
+
+    if (number > 9):
+      for i in set(dico.keys()).difference(set(exclude)):
+        i_modulo = i % number
+        for j in range(7):
+          lcd_array.append(dico[i_modulo + 1][j] + space[j] + dico[i][j])
     else:
-        lcd_array = dico[number]
+      lcd_array = dico[number]
 
     return "\n".join(lcd_array)
-
-
