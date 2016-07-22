7414ddb95c1464086ff74226bc23fe3a7cceae7b
HafidTraikzi <hafid.traikzi@gmail.com>
2016-07-22 16:22:28 +0200

    traitement de deux mots, dans l'ordre

--- markovChain/python/bot_test.py
+++ markovChain/python/bot_test.py
@@ -1,9 +1,14 @@
 import unittest
 
-def bot(text, word_count):
-    if (text == "banane patate"):
-        return "banane"
-    return text
+def bot(text, asked_word_quantity):
+    words = text.split()
+    new_text = ""
+
+    if (asked_word_quantity >= 1):
+        new_text += words[0]
+    if (asked_word_quantity >= 2):
+        new_text += ' ' + words[1]
+    return new_text
 
 class BotTest(unittest.TestCase):
 
@@ -14,7 +19,10 @@ class BotTest(unittest.TestCase):
         self.assertEqual('patate', bot('patate', 1))
         self.assertEqual('banane', bot('banane', 1))
 
-    def test_deux_mot_mais_un_demande(self):
+    def test_deux_mots_mais_un_demande(self):
         self.assertEqual('banane', bot('banane patate', 1))
+
+    def test_deux_mots_mais_deux_demandes(self):
+        self.assertEqual('banane patate', bot('banane patate', 2))
 
 unittest.main()
