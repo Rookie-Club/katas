88af6a4cf65dd57fb72a32827d63fda6bbf84cb0
Yannick Francois <yaf@elsif.fr>
2016-08-05 15:24:48 +0200

    Ajout d'un canvas comme terrain pour la fourmi

--- langton/javascript/langton.js
+++ langton/javascript/langton.js
@@ -1,3 +1,7 @@
 window.onload = function (){
   console.log('je suis chargee !');
+  var contexte = terrain.getContext("2d");
+
+  contexte.fillStyle = "purple";
+  contexte.fillRect(20, 20, 200, 200);
 }
