6af92a13d20ca0a64b48c4d575f34ae2b7d6c2e1
Yannick Francois <yaf@elsif.fr>
2016-08-12 16:06:37 +0200

    Ajout de tests de recette

--- bowlinggame/python/bowling_test.py
+++ bowlinggame/python/bowling_test.py
@@ -15,5 +15,11 @@ class BowlingTest(unittest.TestCase):
     def test_deux_lancer_score_4(self):
         self.assertEqual(4, score([3, 1]))
 
+
+    def _test_recettes(self):
+        self.assertEqual(10, score([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+        self.assertEqual(58, score([1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 3]))
+        self.assertEqual(300, score([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
+
 unittest.main()
 
