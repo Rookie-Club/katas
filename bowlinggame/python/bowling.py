5b75fb1bae23b81e35b9c7d3620cd0e1fd4039b7
Yannick Francois <yaf@elsif.fr>
2016-08-12 17:51:47 +0200

    Extract methode pour plus de lisibilité
    Prise en compte du cas du spare
    
    prise en compte des lancer bonus

--- bowlinggame/python/bowling.py
+++ bowlinggame/python/bowling.py
@@ -1,7 +1,23 @@
 def score(quilles):
     score = 0
 
-    for quille in quilles:
-        score += quille
+    for index, quille in enumerate(quilles):
+        if not lancer_bonus(index):
+            score += quille
+
+        if cas_du_spare(index, quilles):
+            score += quille
 
     return score
+
+def lancer_bonus(index):
+    return index >= 20
+
+def cas_du_spare(index, quilles):
+    return debut_de_manche(index) and dix_tombe_en_deux_coups(index, quilles)
+
+def debut_de_manche(index):
+    return index % 2 == 0
+
+def dix_tombe_en_deux_coups(index, quilles):
+    return index >= 2 and quilles[index - 2] + quilles[index - 1] == 10
--- bowlinggame/python/bowling_test.py
+++ bowlinggame/python/bowling_test.py
@@ -15,10 +15,22 @@ class BowlingTest(unittest.TestCase):
     def test_deux_lancer_score_4(self):
         self.assertEqual(4, score([3, 1]))
 
+    def test_trois_lancer_avec_spare(self):
+        self.assertEqual((3+7+5)+5, score([3, 7, 5]))
 
-    def _test_recettes(self):
-        self.assertEqual(10, score([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
-        self.assertEqual(58, score([1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 3]))
+    def test_trois_lancer_avec_autre_spare(self):
+        self.assertEqual((3+5)+(5+2), score([3, 5, 5, 2]))
+
+    def test_cas_du_spare_avec_lancer_bonus(self):
+        self.assertEqual(True, cas_du_spare(10, [1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 3]))
+
+    def test_recette_que_une_quille_par_manche(self):
+        self.assertEqual(10, score([1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0]))
+
+    def test_recette_que_des_spares(self):
+        self.assertEqual(112, score([1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 1, 9, 3]))
+
+    def test_recette_que_des_strikes(self):
         self.assertEqual(300, score([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]))
 
 unittest.main()
