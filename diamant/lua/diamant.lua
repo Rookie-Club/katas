3228caf7495c6d8fe00b915631f2b0062d0b0786
Yannick Francois <yaf@elsif.fr>
2016-08-26 12:08:55 +0200

    premier tour jusqu'au B

---
+++ diamant/lua/diamant.lua
@@ -0,0 +1,4 @@
+function diamant(lettre)
+  if lettre == "B" then return " AB B A" end
+  return lettre
+end
--- diamant/lua/diamant_test.lua
+++ diamant/lua/diamant_test.lua
@@ -1,6 +1,18 @@
 require 'busted.runner'()
+require 'diamant'
 
-describe("a test", function()
-  -- tests to here
-  
+describe("construire un diamant avec", function()
+
+  it("vide donne vide", function()
+    assert.is_equal("", diamant(""))
+  end)
+
+  it("A donne A", function()
+    assert.is_equal("A", diamant("A"))
+  end)
+
+  it("B donne AB B A", function()
+    assert.is_equal(" AB B A", diamant("B"))
+  end)
+
 end)
