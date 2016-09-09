3cefda6828d772dfcef239f2f639ab0ed33ba76b
Simon <sims38930@gmail.com>
2016-09-09 12:17:14 +0200

    ajoute le fait de pourvoir retirer des batons
    Simon pilote, Yohann navigue

--- nim/nim.py
+++ nim/nim.py
@@ -1,6 +1,9 @@
 class jeuDeNim():
     def __init__(self, batons):
-        pass
+        self.batons = batons
 
-    def nim(batons):
-        return batons
+    def debut(self, batons):
+        return self.batons
+
+    def retirer_des_batons(self, batons, batons_a_retirer):
+        return batons - batons_a_retirer
--- nim/nim_test.py
+++ nim/nim_test.py
@@ -9,6 +9,10 @@ class NimTest(unittest.TestCase):
 
     def test_10_batons_au_depart(self):
         jeu = jeuDeNim(10)
-        self.assertEqual(10, jeu)
+        self.assertEqual(10, jeu.debut(10))
+
+    def test_retirer_1_baton(self):
+        jeu = jeuDeNim(10)
+        self.assertEqual(9, jeu.retirer_des_batons(10, 1))
 
 unittest.main()
