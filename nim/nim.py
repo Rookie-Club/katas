d7e5025dbbcc016735fe3b3447a43cd38ccc2470
Yohann Bosco <bosco.yohann@gmail.com>
2016-09-16 12:34:57 +0200

    Identifie le joueur qui doit jouer
    Simon observe, Yohann pilote, Hafid scribe

--- nim/nim.py
+++ nim/nim.py
@@ -1,10 +1,12 @@
 class Partie():
     def __init__(self, batons):
         self.batons = batons
+        self.joueur = 1
 
     def retirer(self, batons_a_retirer):
         if (batons_a_retirer < self.batons):
             self.batons -= batons_a_retirer
-
-    def joueur(self, numero_joueur):
-        (numero_joueur % 2) + 1
+        if (self.joueur == 1):
+            self.joueur = 2
+        else:
+            self.joueur = 1
--- nim/nim_test.py
+++ nim/nim_test.py
@@ -1,6 +1,7 @@
 import unittest
 from nim import *
 
+
 class NimTest(unittest.TestCase):
 
     def test_partie_initiale_a_10_batons(self):
@@ -17,12 +18,15 @@ class NimTest(unittest.TestCase):
         partie_10_batons.retirer(3)
         self.assertEqual(3, partie_10_batons.batons)
 
-    def test_tour_des_joueurs(self):
+    def test_apres_tour_1_pour_joueur_1(self):
         partie_10_batons = Partie(10)
-        partie_10_batons.joueur(1)
         partie_10_batons.retirer(1)
-        partie_10_batons.joueur(2)
+        self.assertEqual(2, partie_10_batons.joueur)
+
+    def test_tour_2_pour_joueur_2(self):
+        partie_10_batons = Partie(10)
         partie_10_batons.retirer(1)
-        self.assertEqual(2, partie_10_batons.batons)
+        partie_10_batons.retirer(1)
+        self.assertEqual(1, partie_10_batons.joueur)
 
 unittest.main()
