b8e71f93432566e02f6198077233e7dcc120d426
yohann bosco <yohann.bosco@gmail.com>
2016-09-23 12:07:10 +0200

    fait tomber une premiere quille

--- bowlinggame/spec/bowling_spec.js
+++ bowlinggame/spec/bowling_spec.js
@@ -1,5 +1,6 @@
 describe("Jeu du Bowling", function () {
-  it("Test True False", function () {
-    expect(true).toEqual(true);
+  it("fait tomber une quille", function () {
+    let partieBowling = new Object();
+    expect(1).toEqual(partieBowling.quillesTombees);
   });
 });
