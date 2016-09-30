fc572e6414dc0b42013872096081919c6017cf2f
simon <sims38930@gmail.com>
2016-09-30 16:35:54 +0200

    initialise une partie bowling avec un score de 0

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -1,5 +1,8 @@
 describe( "Le bowling cambodgien", function () {
+  it("initialisation de la partie", function () {
+    expect(jeuBowling.score).toEqual(0);
+  });
   it("True False", function () {
-    assertEqual(True, False)
-  };
-  };
+    expect(true).toEqual(true);
+  });
+});
---
+++ src/bowling.js
@@ -0,0 +1,1 @@
+let jeuBowling = {score: 0};
