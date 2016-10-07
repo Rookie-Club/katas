38029a59d7eaea83c5ccd4e92896e89f24f4ccb7
Simon <sims38930@gmail.com>
2016-10-07 17:23:47 +0200

    Fait un spare, permet un bonus avec le lancé d'après

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -7,15 +7,42 @@ describe( "Le bowling cambodgien", function () {
 
   it("une quille tombee, score 1", function () {
     var jeuBowling = new Bowling();
-    jeuBowling.lancee(1);
+    jeuBowling.lance(1);
     expect(jeuBowling.score).toEqual(1);
   });
 
-  it("deuxieme lancer, une quille tombee, score 2", function () {
+  it("avec un spare, bonus avec le prochain lance", function () {
     var jeuBowling = new Bowling();
-    jeuBowling.lancee(1);
-    jeuBowling.lancee(1);
-    expect(jeuBowling.score).toEqual(2);
+    jeuBowling.lance(7);
+    jeuBowling.lance(3);
+    jeuBowling.lance(7);
+    expect(jeuBowling.score).toEqual(24);
   });
 
+  it("avec un spare, bonus avec le prochain lance, même si c'est pas la première manche", function () {
+    var jeuBowling = new Bowling();
+    jeuBowling.lance(0);
+    jeuBowling.lance(2);
+    jeuBowling.lance(7);
+    jeuBowling.lance(3);
+    jeuBowling.lance(2);
+    expect(jeuBowling.score).toEqual(16);
+  });
+
+  it("pas de spare si le total de 10 est sur deux manches", function () {
+    var jeuBowling = new Bowling();
+    jeuBowling.lance(0);
+    jeuBowling.lance(7);
+    jeuBowling.lance(3);
+    jeuBowling.lance(2);
+    expect(jeuBowling.score).toEqual(12);
+  });
+
+  it("on compte le nombre de lance", function (){
+    var jeuBowling = new Bowling();
+    jeuBowling.lance(0);
+    expect(jeuBowling.nombre_de_lance).toEqual(1);
+    jeuBowling.lance(0);
+    expect(jeuBowling.nombre_de_lance).toEqual(2);
+  });
 });
--- src/bowling.js
+++ src/bowling.js
@@ -1,8 +1,17 @@
 var Bowling = function () {
   this.score = 0;
+  this.nombre_de_lance = 0;
 };
 
-Bowling.prototype.lancee = function (quilles) {
+Bowling.prototype.lance = function (quilles) {
+  if (this.spare()) {
+    this.score += quilles;
+  }
   this.score += quilles;
+  this.nombre_de_lance += 1;
+};
+
+Bowling.prototype.spare = function () {
+  return this.score >= 10 && this.nombre_de_lance % 2 == 0;
 };
 
