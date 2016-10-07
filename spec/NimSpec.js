670b590cc2f05eefb88220c49ef691674e4187e5
Yohann Bosco <yohann.bosco@gmail.com>
2016-10-07 11:51:24 +0200

    Retire 3 batons max

--- spec/NimSpec.js
+++ spec/NimSpec.js
@@ -1,7 +1,18 @@
 describe("Nim", function() {
   it("contient 10 bâtons", function() {
     var partie = new Nim(10);
-    expect(10).toEqual(partie.batons_restants);
+    expect(partie.batons_restants).toEqual(10);
+  });
+
+  it("retire 1 bâton", function() {
+    var partie = new Nim(10);
+    partie.retire(1);
+    expect(partie.batons_restants).toEqual(9);
+  });
+
+  it("retire 3 bâtons max", function() {
+    var partie = new Nim(10);
+    expect(partie.retire(4)).toEqual(false);
   });
 });
 
