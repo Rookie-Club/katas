2aebab19e46df03846cc5ef4388d9aa205b6ee06
Yohann Bosco <yohann.bosco@gmail.com>
2016-10-07 11:51:24 +0200

    Retire 3 batons max

--- src/Nim.js
+++ src/Nim.js
@@ -1,3 +1,10 @@
 var Nim = function (batons_au_depart) {
-  return { batons_restants: batons_au_depart };
+  return {
+    batons_restants: batons_au_depart,
+    retire: function (batons_a_retirer) {
+      if (batons_a_retirer > 3) { return false; };
+      this.batons_restants = this.batons_restants - batons_a_retirer;
+      return true;
+    }
+  };
 };
