0e1d6c48f225cf9dcd30473a1edeeed839dab5d9
Hafid Traikzi <hafid.traikzi@gmail.com>
2016-10-14 12:48:12 +0200

    Tentative de résolution du test rouge : la fourmi a une position initiale

--- src/langton.js
+++ src/langton.js
@@ -3,4 +3,8 @@ window.onload = function () {
   var context = canvas.getContext("2d");
   context.fillStyle = "red";
   context.fillRect(140, 140, 20, 20);
+
+  var Fourmi = function () {
+    var position = {x: 0, y: 0};
+  };
 };
