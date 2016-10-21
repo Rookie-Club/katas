a39095add361205e0b36abb1269fce2133879cc7
HafidTraikzi <hafid.traikzi@gmail.com>
2016-10-21 12:39:34 +0200

    Mise en état fonctionnel

--- bindings.js
+++ bindings.js
@@ -1,4 +1,4 @@
-window.antWorld = function () {
+window.onload = function () {
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
   context.fillStyle = "black";
