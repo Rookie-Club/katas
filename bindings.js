c58030e863b23c7bba77738c6548096543db6171
HafidTraikzi <hafid.traikzi@gmail.com>
2016-10-21 12:58:54 +0200

    Utilise la fourmi pour l'affichage

--- bindings.js
+++ bindings.js
@@ -1,6 +1,25 @@
+const taille_fourmi = 20;
+var world_width, world_height;
+
 window.onload = function () {
   var canvas = document.getElementById("canvas");
   var context = canvas.getContext("2d");
-  context.fillStyle = "black";
-  context.fillRect(140, 140, 20, 20);
+  world_width = canvas.width;
+  world_height = canvas.height;
+
+  var fourmi = new Ant();
+  dessine_fourmi(fourmi, context);
 };
+
+const dessine_fourmi = function (fourmi, context) {
+  context.fillStyle = "black";
+  position = decalage_position(fourmi);
+  context.fillRect(position.x, position.y, taille_fourmi, taille_fourmi);
+}
+
+const decalage_position = function (fourmi) {
+  return {
+    x: fourmi.position.x - (taille_fourmi / 2) + world_width / 2,
+    y: fourmi.position.y - (taille_fourmi / 2) + world_height / 2
+  };
+}
