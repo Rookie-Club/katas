f040507e0ff7989910f6f1bdb86b3f7f4fe03820
HafidTraikzi <hafid.traikzi@gmail.com>
2016-11-18 17:32:21 +0100

    Ajoute le sens de l'orientation à la fourmi

--- src/langton.js
+++ src/langton.js
@@ -2,36 +2,32 @@ const Ant = function () {
 
   this.position = {x: 0, y: 0};
   this.direction = {x: 0, y: -1};
+  this._orientation = "North";
   this.colorCase = "white";
 
   this.move = function () {
-    if (this.position.x == 1) {
-      this.position.x += this.direction.x;
-      this.position.y += this.direction.y;
-      this.direction = {x: -1, y: 0};
-    } else if (this.position.y == -1) {
-      this.position.x += this.direction.x;
-      this.position.y += this.direction.y;
-      this.direction = {x: 0, y: 1};
-    } else {
-      this.position.x += this.direction.x;
-      this.position.y += this.direction.y;
+    this.position.x += this.direction.x;
+    this.position.y += this.direction.y;
+    this.tourne_a_droite();
+  }
+
+  this.tourne_a_droite = function () {
+    if (this.direction.x == 0 && this.direction.y == -1) {
+      this._orientation = "East";
       this.direction = {x: 1, y: 0};
+    } else if (this.direction.x == 1 && this.direction.y == 0) {
+      this._orientation = "South";
+      this.direction = {x: 0, y: 1};
+    } else if (this.direction.x == 0 && this.direction.y == 1) {
+      this._orientation = "West";
+      this.direction = {x: -1, y: 0};
+    } else if (this.direction.x == -1 && this.direction.y == 0) {
+      this._orientation = "North";
+      this.direction = {x: 0, y: -1};
     }
   }
 
   this.orientation = function () {
-    if (this.direction.x == 0 && this.direction.y == -1) {
-      return "North";
-    }
-    if (this.direction.x == 1 && this.direction.y == 0) {
-      return "East";
-    }
-    if (this.direction.x == 0 && this.direction.y == 1) {
-      return "South";
-    }
-    if (this.direction.x == -1 && this.direction.y == 0) {
-      return "West";
-    }
+    return this._orientation;
   }
 }
