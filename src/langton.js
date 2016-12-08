d894084b3532726c2d29c7bf4e505c58f5fd80d9
HafidTraikzi <hafid.traikzi@gmail.com>
2016-12-08 14:56:59 +0100

    Pose le monde de la fourmi de Langton

--- src/langton.js
+++
@@ -1,7 +0,0 @@
-const nouvellePositionFourmi = function (positionFourmi, orientation) {
-    return [positionFourmi[0] + orientation[0], positionFourmi[1] + orientation[1]];
-}
-
-const nouvelleOrientationFourmi = function (positionCourante, orientation, casesNoires) {
-  return [1, 0];
-}
