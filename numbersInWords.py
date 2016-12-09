4da65366c246ce2d340b3320ae93da59072759f4
Simon <sims38930@gmail.com>
2016-12-09 17:41:33 +0100

    Utilise une vingtaine
    - Il va falloir beaucoup de chiffres dans le dictionnaire
    - Difficile d'explorer le sujet
    - Tendance à faire un trop gros pas (directement une boucle)
    - Une activitée importante en programmation consiste à découper un
      problème en plusieurs problèmes plus petit.

--- numbersInWords.py
+++ numbersInWords.py
@@ -1,4 +1,6 @@
 
 def inWords(number):
-    dico = {1: "un", 2: "deux", 3: "trois"}
+    dico = {1: "un", 2: "deux", 3: "trois", 20: "vingt"}
+    if number == 23:
+        return dico[20] + " " + dico[3]
     return dico[number]
--- numbersInWords_test.py
+++ numbersInWords_test.py
@@ -14,4 +14,7 @@ class NumbersInWordsTest(unittest.TestCase):
     def test_3_vaut_trois(self):
         self.assertEqual("trois", inWords(3))
 
+    def test_23_vaut_vingt_trois(self):
+        self.assertEqual("vingt trois", inWords(23))
+
 unittest.main()
