e40ef49ca9198f09213e20ce871eeeff2da6e868
Simon <sims38930@gmail.com>
2016-12-09 16:50:25 +0100

    Utilise un dictionnaire pour mapper les chiffres
    - Rappel sur l'usage d'un dictionnaire en python `dico[maclef]`
    - On a vu un message d'erreur qui parlait d'un problème sur un type :
      `TypeError: 'dict' object is not callable`.
    - Dans un langage utilisant un typage dynamique, on ne vois pas
      apparaitre le type des objets utilisé, difficile de faire le
      rapprochement ensuite quand on les vois dans un message d'erreur par
      exemple.
    - Ça fait parti des choses qu'on apprend à force de manipuler.
    
    En Ruby
    ```Ruby
    irb(main):002:0> dico = {1 => "un", 2 => "deux"}
    irb(main):006:0> dico(1)
    NoMethodError: undefined method `dico' for main:Object
            from (irb):6
            from /usr/bin/irb:11:in `<main>'
    ```
    
    En Python
    ```Python
    >>> dico = {1: "un"}
    >>> dico(1)
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
    TypeError: 'dict' object is not callable
    ```
    
    En JavaScript
    ```JavaScript
    a = {x: "un", y: "deux"}
    a('x')
    TypeError: a is not a function
    ```
    
    On observe que JavaScript comme Python utilise la référence derrière la
    variable, et tente de l'executer, alors qu'en Ruby, on cherche une
    méthode qui porterais le nom demandé.

--- numbersInWords.py
+++ numbersInWords.py
@@ -1,5 +1,4 @@
 
-def inWords(numbers):
-    if numbers == 2:
-        return "deux"
-    return "un"
+def inWords(number):
+    dico = {1: "un", 2: "deux", 3: "trois"}
+    return dico[number]
--- numbersInWords_test.py
+++ numbersInWords_test.py
@@ -11,4 +11,7 @@ class NumbersInWordsTest(unittest.TestCase):
     def test_2_vaut_deux(self):
         self.assertEqual("deux", inWords(2))
 
+    def test_3_vaut_trois(self):
+        self.assertEqual("trois", inWords(3))
+
 unittest.main()
