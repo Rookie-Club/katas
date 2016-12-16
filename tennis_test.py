8ea2226049c334313c27f8a4fea999b462fa2d3e
meo84 <oopsiday@gmail.com>
2016-12-16 12:37:10 +0100

    Deuxième point marqué
    - C'est super de voir Marine tenter autre chose durant la cuisine.
    - En TDD on code pas forcement la bonne solution en premier, c'est
      perturbant.
    - On a pas revu les cycles du TDD.
    - L'internet fonctionne vraiment pas.
    - Y'a beaucoup de commandes en vim.
    - «Cette discussion me fait penser au méta-code» --Hafid

--- tennis_test.py
+++ tennis_test.py
@@ -1,10 +1,16 @@
 import unittest
 
-def score(points):
-    return [15, 0]
+def score(winners):
+    if winners == ['j1']:
+        return [15, 0]
+    elif winners == ['j1', 'j1']:
+        return [30, 0]
 
 class TennisTest(unittest.TestCase):
     def test_joueur_1_marque_premier_point(self):
         self.assertEqual([15, 0], score(['j1']))
+
+    def test_joueur_1_marque_second_point(self):
+        self.assertEqual([30, 0], score(['j1', 'j1']))
 
 unittest.main()
