10ecce6228d03c9aceb21059268b16ebefab2fd7
meo84 <oopsiday@gmail.com>
2016-12-16 15:17:09 +0100

    Joue à deux au tennis
    - En vim :
            - `dd` et `35dd`
            - `ce`, `cw`, `c$`
    - En python `len(monArray)` et `for element in monArray`.

--- tennis_test.py
+++ tennis_test.py
@@ -1,10 +1,13 @@
 import unittest
 
+def point(index):
+    score_range = [0, 15, 30, 40]
+    return score_range[index]
+
 def score(winners):
-    if winners == ['j1']:
-        return [15, 0]
-    elif winners == ['j1', 'j1']:
-        return [30, 0]
+    if winners == ["j2"]:
+        return [0, point(len(winners))]
+    return [point(len(winners)), 0]
 
 class TennisTest(unittest.TestCase):
     def test_joueur_1_marque_premier_point(self):
@@ -12,5 +15,11 @@ class TennisTest(unittest.TestCase):
 
     def test_joueur_1_marque_second_point(self):
         self.assertEqual([30, 0], score(['j1', 'j1']))
+
+    def test_joueur_1_marque_troisieme_point(self):
+        self.assertEqual([40, 0], score(['j1', 'j1', 'j1']))
+
+    def test_joueur_2_marque_premier_point(self):
+        self.assertEqual([0, 15], score(['j2']))
 
 unittest.main()
