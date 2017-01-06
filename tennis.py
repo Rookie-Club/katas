6b248489afca0b9660bd5fc81a1781034a5c6730
meo84 <oopsiday@gmail.com>
2017-01-06 17:27:09 +0100

    Started a match

--- tennis.py
+++ tennis.py
@@ -1,1 +1,14 @@
+class Match():
 
+  def __init__(self, player1, player2):
+    self.player1 = player1
+    self.player2 = player2
+
+  def points(self, player):
+    return 0
+
+  def jeux(self, player):
+    return 0
+
+  def sets(self, player):
+    return 0
