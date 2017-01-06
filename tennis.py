a5904acc77e51f47edb0fa04de38175f8f30edb6
Simon <sims38930@gmail.com>
2017-01-06 12:05:42 +0100

    Calcul du premier set pour un joueur gagnant tout
    - En Vim, on peut faire `yi(` quand on est dans des parenthèses.
    - Du mal à avancer, trouver la voie, le chemin à prendre. Est-ce parce
      que nous n'allons pas jusqu'au bout de nos idées ? Pas d'alignement de
      décision d'équipe.
    - C'est vraiment important de revenir en arrière
    - «C'est bien de `U`ter» -- Simon

--- tennis.py
+++ tennis.py
@@ -8,8 +8,14 @@ def game_score_calculus(winners):
 
     for winner in winners:
         game_score[winner] += 1
+        game_score[winner] = game_score[winner] % 4
 
     return game_score
 
 def game_score(winners):
-   return show_game_points(game_score_calculus(winners))
+    return show_game_points(game_score_calculus(winners))
+
+def set_score(winners):
+    if winners == ['j2', 'j2', 'j2', 'j2', 'j2', 'j2', 'j2', 'j2']:
+        return [2, 0]
+    return [1, 0]
--- tennis_test.py
+++ tennis_test.py
@@ -21,7 +21,14 @@ class TennisTest(unittest.TestCase):
     def test_player_1_wins_2_times_player_2_wins_1_time(self):
         self.assertEqual([30, 15], game_score(['j2', 'j1', 'j1']))
 
-    def xtest_player_1_wins_4_times(self):
-        self.assertEqual([30, 15], game_score(['j2', 'j1', 'j1']))
+    def test_player_1_wins_4_times(self):
+        winners = ['j2', 'j2', 'j2', 'j2']
+        self.assertEqual([0, 0], game_score(winners))
+        self.assertEqual([1, 0], set_score(winners))
+
+    def test_player_1_wins_8_times(self):
+        winners = ['j2', 'j2', 'j2', 'j2', 'j2', 'j2', 'j2', 'j2']
+        self.assertEqual([0, 0], game_score(winners))
+        self.assertEqual([2, 0], set_score(winners))
 
 unittest.main()
