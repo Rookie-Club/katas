92b7ee67031c6cffe9167d22ab70f90a6bfd58c3
Yannick Francois <yaf@elsif.fr>
2017-01-09 22:13:39 +0100

    La fourmi de langton sur une branche

--- langton/cpp/langton_test.cpp
+++
@@ -1,9 +0,0 @@
-#define BOOST_TEST_MAIN
-#include <boost/test/unit_test.hpp>
-#include "langton.h"
-
-BOOST_AUTO_TEST_CASE(position_initial_de_la_fourmi) {
-  Ant ant;
-  BOOST_REQUIRE_EQUAL(0, ant.position().x);
-  BOOST_REQUIRE_EQUAL(0, ant.position().y);
-}
