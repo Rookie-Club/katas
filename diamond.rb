e52beddfeffaa2997127d58aee9248839e3e43a8
meo84 <oopsiday@gmail.com>
2017-02-03 17:18:48 +0100

    Drew B and C diamonds
    - Après une première approche brut force, on essaie de trouver comment
      décomposer.

--- diamond.rb
+++ diamond.rb
@@ -1,3 +1,5 @@
 def draw_diamond(letter)
-  letter
+  return 'A\nB B\nA' if letter == 'B'
+  return 'A\nB B\nC  C\nB B\nA' if letter == 'C'
+  return letter if letter == 'A'
 end
--- test_diamond.rb
+++ test_diamond.rb
@@ -7,5 +7,14 @@ class TestDiamond < Minitest::Test
     assert_equal 'A', draw_diamond('A')
   end
 
+  def test_B_diamond
+    assert_equal 'A\nB B\nA', draw_diamond('B')
+  end
+
+  def test_C_diamond
+    assert_equal 'A\nB B\nC  C\nB B\nA', draw_diamond('C')
+  end
+
+
 end
 
