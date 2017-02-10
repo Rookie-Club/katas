3ae89dbb54d616bc3222c65a12cd5ebd19b3a027
Yohann Bosco <yohann.bosco@gmail.com>
2017-02-10 16:13:38 +0100

    créé des routes vers les 4 pages de simon.py
    - Ne pas oublier de relancer le serveur pour prendre en compte les
      modifications

--- simon.py
+++ simon.py
@@ -3,8 +3,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return "Hello World!"
+
+@app.route("/upload")
+def upload():
+    return "on est sur la page upload"
+
+@app.route("/references")
+def references():
+    return "on est sur la page references"
+
+@app.route("/generate")
+def generate():
+    return "on est sur la page generate"
 
 if __name__ == "__main__":
     app.run()
