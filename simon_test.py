2f4d61b3c90c50026b5ad1eacc1c55e87142ff94
Yannick François <yaf@elsif.fr>
2017-02-10 16:29:15 +0100

    Ajout de tests

---
+++ simon_test.py
@@ -0,0 +1,28 @@
+import os
+import simon
+import unittest
+import tempfile
+
+class SimonTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = simon.app.test_client()
+
+    def test_home_page(self):
+        page = self.app.get('/')
+        self.assertEqual(b'Hello World!', page.data)
+
+    def test_upload_page(self):
+        page = self.app.get('/upload')
+        self.assertEqual(b'on est sur la page upload', page.data)
+
+    def test_references_page(self):
+        page = self.app.get('/references')
+        self.assertEqual(b'on est sur la page references', page.data)
+
+    def test_generate_page(self):
+        page = self.app.get('/generate')
+        self.assertEqual(b'on est sur la page generate', page.data)
+
+if __name__ == '__main__':
+    unittest.main()
