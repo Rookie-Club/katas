ca9066264be75ea1dd1b79c12046684c67eff8f4
Yannick Francois <yaf@elsif.fr>
2017-02-10 11:51:52 +0100

    Eclaircissement du code
    - Extraire une variable sert à améliorer la visibilité, mais permet aussi
      de ne faire une opération qu'une fois.
    - Un programme qui fonctionne c'est pas mal, mais pour bien faire il
      faudrait qu'il soit lisible.
    - Un kata c'est pas juste résoudre le problème, mais s'entrainer à
      écrire du code propre.
    - La manipulation avec `ord(letter)` et `chr(65)` semble un peu
      compliqué.

--- diamant.py
+++ diamant.py
@@ -1,17 +1,21 @@
 def diamant(lettre):
-    k = ord(lettre) - 65
-    triangle = [k*" " + chr(65)]
-    ligne = 1
-    while (ligne < k+1):
-        triangle.append((k - ligne) * " " +chr(65 + ligne) + (2 * (ligne - 1) +1) * " " + chr(65 + ligne))
-        ligne += 1
+    indice_lettre = ord(lettre) - 65
+    triangle = [indice_lettre * " " + "A"]
+
+    for ligne in range(1, indice_lettre + 1):
+        nombre_espace_gauche = indice_lettre - ligne
+        nombre_espace_interne = 2 * ligne - 1
+        lettre_courante = chr(65 + ligne)
+        triangle.append(nombre_espace_gauche * " " + lettre_courante +
+                        nombre_espace_interne * " " + lettre_courante)
+
     triangle_inverse = list(reversed(triangle))
     diamant = triangle + triangle_inverse[1:]
     return "\n".join(diamant)
 
 if __name__ == "__main__":
     while True:
-        lettre = (input("Entrez une lettre\n")).upper()
+        lettre = input("Entrez une lettre\n").upper()
         print(diamant(lettre))
         continuer = input("Jouer encore? (y/n)\n")
         if continuer == "n":
