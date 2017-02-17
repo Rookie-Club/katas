6dce9717b9bed1d3dcd420f42fa253ac92f9178f
Yannick Francois <yaf@elsif.fr>
2017-02-17 16:01:51 +0100

    Nettoyage du code
    Hafid a réalisé cette exercices _offline_. Jolie travail :-)
    
    Perfection game de ses deux camarades:
    
    > Ce qui nous plait.
    >
    > Simon :
    >
    > - Le missile et la maison.
    > - Que le proramme soit réalisé avec la solution que voulait utiliser Hafid (tableau de lettres).
    >
    >
    > Marine :
    >
    > - Une interface complête pour jouer.
    > - Le langage de l'interface adapté au contexte du jeu.
    > - Le programme écrit avec des noms anglais (variables, méthodes, ...).
    >
    >
    > Ce qu'il faudrait faire pour que ce soit mieux.
    >
    > Simon :
    >
    > - Au lieu de deux `if`, un pour "A" et un pour le reste, il y avait un `if (...) else`.
    > - Ça aurait été chouette que les minuscules soient prises en compte.
    >
    >
    > Marine :
    >
    > - Trouver un nom plus précis pour les variables `space_left` et `space_right`.
    > - Il pourrait y avoir une duplication en moins `letter_list.append(i.rjust(space_left))`.
    > - Séparer plus les responsabilités (et du coup avoir plus de tests).
    
    Debrief :
    
    - C'est important de se relire après une pause, pour prendre du recul sur le code.
    - C'est bien de faire un commentaire de code en mode perfection game.

--- diamond.py
+++ diamond.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python
 # ** coding=utf8 **
 
+import string
+
 
 def diamond(letter):
+    return build_representation_of(build_diamond(letter))
 
-  alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+def build_diamond(letter):
+  alphabet = list(string.uppercase)
   space_left = 0
   space_right = 0
   letter_list = []
-  diamond = ""
 
-  for index, _letter in enumerate(alphabet):
-    if letter in _letter:
-      for i in alphabet[:index + 1]:
-        if i in "A":
-          space_left += index + 1
-          letter_list.append(i.rjust(space_left))
-        if i not in "A":
-          space_left -= 1
-          space_right += 2
-          letter_list.append(i.rjust(space_left) + i.rjust(space_right))
-      diamond = "\n".join(letter_list) + "\n".join(reversed(letter_list)).replace(letter_list[-1], "")
-      print diamond
-      return diamond
+  index = alphabet.index(letter)
+  for i in alphabet[:index + 1]:
+    if i in "A":
+      space_left += index + 1
+      letter_list.append(i.rjust(space_left))
+    else:
+      space_left -= 1
+      space_right += 2
+      letter_list.append(i.rjust(space_left) + i.rjust(space_right))
+  return letter_list
+
+
+def build_representation_of(array_diamond):
+  return "\n".join(array_diamond) + "\n".join(reversed(array_diamond)).replace(array_diamond[-1], "")
+
 
 if __name__ == "__main__":
 
@@ -32,7 +38,7 @@ if __name__ == "__main__":
   if letter:
     draw = raw_input("Tu as pensé à la lettre " + letter + ". Maintenant, dessinons un diamant jusqu'à la lettre choisi. Entre (y) pour voir le diamant ou (n) pour lancer un missile : ")
   if draw == "y":
-      diamond(letter)
+      print diamond(letter)
   if yes == "n" or draw == "n":
     raw_input("                      ____________\n"
            "   \____________      | []    [] |\n"
