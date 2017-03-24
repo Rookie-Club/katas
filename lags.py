001d4ef18e9b52453d0d1dd39ba3957c5bbf252b
Simon <sims38930@gmail.com>
2017-03-24 17:46:06 +0100

    lags new try

--- lags.py
+++ lags.py
@@ -1,35 +1,45 @@
-
-
-def check_fly(fly_list):
-    if (fly_list == []):
-        return None
-    else:
+def check_flys(fly_list):
         return True
 
-def check_max_gain(fly_list):
-    gain_max = 0
-    gain_list = []
+def check_gain_max(fly_list):
+    fly_gain = []
+    fly_gain_max = []
 
     for fly in fly_list:
-        gain_list.append(fly[-1])
-    gain_max = max(gain_list)
-    return gain_max
+        fly_gain.append(fly[-1])
+    fly_gain_max = max(fly_gain)
+    return fly_gain_max
 
 def check_hours(fly_list):
-    fly_begin = []
-    fly_ending = []
-    fly_hours = []
+    flys_compatibility = []
+    flys_hours = []
+    fly_hours_tmp = []
+    index = 0
 
     for fly in fly_list:
-        fly_begin.append(fly[1])
-        fly_ending.append(fly[1] + fly[2])
-        fly_hours.append(fly_begin, fly_ending)
-    return fly_hours
+        fly_begin = fly_list[index][1]
+        fly_ending = fly_list[index][1] + fly_list[index][2]
+        fly_hours_tmp.append(fly_begin)
+        fly_hours_tmp.append(fly_ending)
+        flys_hours.append(fly_hours_tmp)
+        fly_hours_tmp = []
+        index += 1
+    return flys_hours
 
-def check_fly_compatibility(fly_list, fly_hours):
-    compatibility_list = []
+def check_compatibility(fly_list):
+    flys_hours = check_hours(fly_list)
+    fly_compatibility = []
+    fly_compatibility_tmp = [0]
+    base = flys_hours[0][1]
+    index = 1
 
-    for fly in fly_hours:
-        if fly[0][0] > fly[1][-1]:
-            compatibility_list.append(fly_list[0], fly_list[1])
-    return compatibility_list
+    for fly in flys_hours:
+        if flys_hours[index][0] >= base:
+            fly_compatibility_tmp.append(index)
+            base = flys_hours[index][1]
+        else:
+            flys_hours.remove(flys_hours[0])
+            fly_compatibility.append(fly_compatibility_tmp)
+            index += 1
+
+    return fly_compatibility
--- lags_test.py
+++ lags_test.py
@@ -1,28 +1,25 @@
-
 import unittest
 from lags import *
 
-
 class LagsTest(unittest.TestCase):
-
     def setUp(self):
         self.fly_list = [["AF514", 0, 5, 10], 
             ["CO5", 3, 7, 14], 
             ["AF515", 5, 9, 7], 
             ["BA01", 6, 9, 8]]
 
-    def test_we_got_a_fly(self):
-        self.assertEqual(True, check_fly(self.fly_list))
+    def test_check_fly(self):
+        self.assertEqual(True, check_flys(self.fly_list))
 
-    def test_check_max_price(self):
-        self.assertEqual(14, check_max_gain(self.fly_list))
+    def test_check_gain_max(self):
+        self.assertEqual(14, check_gain_max(self.fly_list))
 
     def test_check_hours(self):
-        self.assertEqual([[0, 5], [3, 10], [5, 14], [6, 15]], check_hours(self.fly_list))
+        self.assertEqual([[0, 5],[3, 10], [5, 14], [6, 15]], check_hours(self.fly_list))
 
     def test_check_compatibility(self):
-        self.assertEqual([[0, 2], [0, 3]], check_fly_compatibility(self.fly_list))
+        self.assertEqual([[0, 2], [0, 3]], check_compatibility(self.fly_list))
 
 
-if __name__ == "__main__":
-    unittest.main()
+
+unittest.main()
