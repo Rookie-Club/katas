c3a1bd7f6beba1f7971a5147cc1db8b0b6938d9c
Simon Lebogoss <sims38930@gmail.com>
2017-04-21 17:18:23 +0200

    On essaye de refactorer le calcul de score d'une cellule

--- gol.py
+++ gol.py
@@ -1,12 +1,22 @@
 def transform(grille):
-    return [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+    nouvelle_grille = grille
+    for i, ligne in enumerate(grille):
+        for j, colonne in enumerate(ligne):
+            nouvelle_grille[i][j] = vie_ou_mort(grille, i, j)
+    return nouvelle_grille
 
-def vie_ou_mort(grille, cellule):
-    vie = False
-    if cellules_autour(grille, cellule) == 3:
-        vie = True
+def vie_ou_mort(grille, ligne, colonne):
+    vie = 0
+    if cellules_autour(grille, ligne, colonne) == 2:
+        vie = grille[ligne][colonne]
+    if cellules_autour(grille, ligne, colonne) == 3:
+        vie = 1
     return vie
 
-def cellules_autour(grille, cellule):
-    if cellule == grille[2][1] or cellule == grille[1][1] or cellule == grille[0][1]:
-       return  grille[1][0] + grille[1][1] + grille[1][2]
+def cellules_autour(grille, ligne, colonne):
+    score_cellule = 0
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if 0 <= ligne + i - 1 <= 2 and 0 <= colonne + j - 1 <= 2:
+                score_cellule += grille[ligne + i - 1][colonne + j - 1]
+    return score_cellule - grille[ligne][colonne]
--- test_gol.py
+++ test_gol.py
@@ -7,13 +7,22 @@ class TestGOL(unittest.TestCase):
     grille_etat_2 = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
 
     def test_avec_grille_init_2_cellules_naissent(self):
-        self.assertEqual(vie_ou_mort(self.grille_init, self.grille_init[0][1]), True)
-        self.assertEqual(vie_ou_mort(self.grille_init, self.grille_init[2][1]), True)
+        self.assertEqual(vie_ou_mort(self.grille_init, 0, 1), 1)
+        self.assertEqual(vie_ou_mort(self.grille_init, 2, 1), 1)
 
     def test_avec_grille_init_2_cellules_meurent(self):
-        self.assertEqual(vie_ou_mort(transform(self.grille_init), self.grille_init[1][2]), False)
-        self.assertEqual(vie_ou_mort(transform(self.grille_init), self.grille_init[1][0]), False)
+        self.assertEqual(vie_ou_mort(self.grille_init, 1, 2), 0)
+        self.assertEqual(vie_ou_mort(self.grille_init, 1, 0), 0)
+    def test_avec_grille_etat_2_et_2_cellules_naissent(self):
+        self.assertEqual(vie_ou_mort(self.grille_etat_2, 1, 2), 1)
+        self.assertEqual(vie_ou_mort(self.grille_etat_2, 1, 0), 1)
 
+    def test_score_cellule_donne_3_si_3_cellules_autour(self):
+        self.assertEqual(cellules_autour(self.grille_init, 0, 1), 3)
+        self.assertEqual(cellules_autour(self.grille_init, 2, 1), 3)
+
+print (transform(TestGOL.grille_init))
+print (transform(TestGOL.grille_etat_2))
 
 unittest.main()
 
