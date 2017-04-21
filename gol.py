595d32f215b44c9e7d459b2d4f6231d64388e116
Sims Le bogoss <sims38930@gmail.com>
2017-04-21 18:23:00 +0200

    tente d'afficher une transformation de tableau
    - On a fait du refactoring. On est allé un peu trop loin et on a plus
      fai de test pendant un bon moment.
    - On a eu un souci avec un `print`dans le fichier test qui nous a pris
      plus d'une heure.
    - On a bien effectué le découpage au niveau du code, mais pas au niveau
      des tests.
    - Les tableaux qui sortent ne correspondent pas aux (bons) résultats de
      l'algorithme sous-jacent.

--- gol.py
+++ gol.py
@@ -1,7 +1,7 @@
 def transform(grille):
     nouvelle_grille = grille
-    for i, ligne in enumerate(grille):
-        for j, colonne in enumerate(ligne):
+    for i in range(0, 3):
+        for j in range(0, 3):
             nouvelle_grille[i][j] = vie_ou_mort(grille, i, j)
     return nouvelle_grille
 
@@ -15,8 +15,16 @@ def vie_ou_mort(grille, ligne, colonne):
 
 def cellules_autour(grille, ligne, colonne):
     score_cellule = 0
-    for i in range(0, 3):
-        for j in range(0, 3):
-            if 0 <= ligne + i - 1 <= 2 and 0 <= colonne + j - 1 <= 2:
-                score_cellule += grille[ligne + i - 1][colonne + j - 1]
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if 0 <= ligne + i <= 2 and 0 <= colonne + j <= 2:
+                    score_cellule += grille[ligne + i][colonne + j]
     return score_cellule - grille[ligne][colonne]
+
+if __name__ == "__main__":
+    reponse = input("grille A ou B?\n")
+    if reponse == "A":
+        grille = [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
+    else :
+        grille = [[0, 1, 0], [0, 1, 0], [0, 1, 0]]
+    print(transform(grille))
