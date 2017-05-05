1a3c59389b27d9076eab3ea380e98628680f0fd1
Yohann Bosco <yohann.bosco@gmail.com>
2017-05-05 15:44:11 +0200

    commit initiale avec test qui fonctionne

---
+++ spec/SpecHelper.js
@@ -0,0 +1,15 @@
+ch(function () {
+    jasmine.addMatchers({
+          toBePlaying: function () {
+                  return {
+                            compare: function (actual, expected) {
+                                        var player = actual;
+
+                                                  return {
+                                                                pass: player.currentlyPlayingSong === expected && player.isPlaying
+                                                                            };
+                                                          }
+                                           };
+                      }
+                         });
+});
---
+++ spec/langton_spec.js
@@ -0,0 +1,5 @@
+describe( "la fourmi de langton", function () {
+  it("true is true", function () {
+    expect(true).toEqual(true);
+  });
+});
