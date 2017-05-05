f9e8dc8ca83e7b0888936d1bd1562c57903ef921
Yohann Bosco <yohann.bosco@gmail.com>
2017-05-05 16:41:38 +0200

    la fourmi existe et peut avancer

--- spec/langton_spec.js
+++ spec/langton_spec.js
@@ -1,5 +1,7 @@
-describe( "la fourmi de langton", function () {
-  it("true is true", function () {
-    expect(true).toEqual(true);
+describe( "La Fourmi de Langton", function () {
+  it("est sur une case blanche, dirigée vers le Nord et se déplace à l'Est", function () {
+    var fourmi = new Fourmi([3, 4], "Nord", []);
+    fourmi.avance()
+    expect(fourmi.position_actuelle).toEqual([3, 5]);
   });
 });
---
+++ src/langton.js
@@ -0,0 +1,8 @@
+function Fourmi (position_initiale, direction_initiale, cases_noires) {
+  this.position_actuelle = position_initiale
+  this.direction_actuelle = direction_initiale
+  this.cases_noires = cases_noires
+  this.avance = function () {
+  this.position_actuelle = [3, 5]
+  }
+};
