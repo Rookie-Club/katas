973803c685bcafba6ff61bc2834800dc470de1af
Yohann Bosco <yohann.bosco@gmail.com>
2017-05-08 19:36:12 +0200

    la fourmi change la couleur de la case occupée

--- spec/langton_spec.js
+++ spec/langton_spec.js
@@ -4,4 +4,27 @@ describe( "La Fourmi de Langton", function () {
     fourmi.avance()
     expect(fourmi.position_actuelle).toEqual([3, 5]);
   });
+  it("est sur une case blanche, dirigée vers le Nord et colore la case en noir et se dirige à l'Est", function () {
+    var fourmi = new Fourmi([3, 4], "Nord", []);
+    fourmi.avance()
+    expect(fourmi.direction_actuelle).toEqual("Est");
+    expect(fourmi.cases_noires).toEqual([[3,4]]);
+  });
+  it("est sur une case noire, dirigée vers l'Ouest et colore la case en blanc et se dirige au Sud", function () {
+    var fourmi = new Fourmi([3, 4], "Ouest", [[3, 4]]);
+    fourmi.avance()
+    expect(fourmi.position_actuelle).toEqual([4, 4]);
+    expect(fourmi.direction_actuelle).toEqual("Sud");
+    expect(fourmi.cases_noires).toEqual([]);
+  });
+  it("une case noire devient blanche", function () {
+    var fourmi = new Fourmi([3, 4], "Ouest", [[3, 4]]);
+    fourmi.change_couleur()
+      expect(fourmi.cases_noires).toEqual([]);
+  });
+  it("une case blanche devient noire", function () {
+    var fourmi = new Fourmi([3, 4], "Ouest", []);
+    fourmi.change_couleur()
+      expect(fourmi.cases_noires).toEqual([[3, 4]]);
+  });
 });
--- src/langton.js
+++ src/langton.js
@@ -1,8 +1,24 @@
 function Fourmi (position_initiale, direction_initiale, cases_noires) {
-  this.position_actuelle = position_initiale
-  this.direction_actuelle = direction_initiale
-  this.cases_noires = cases_noires
+  this.position_actuelle = position_initiale;
+  this.direction_actuelle = direction_initiale;
+  this.cases_noires = cases_noires;
+
   this.avance = function () {
-  this.position_actuelle = [3, 5]
+        this.position_actuelle = [3, 5];
+        this.direction_actuelle = "Est";
+        this.cases_noires = [[3, 4]];
+  };
+
+  this.change_couleur = function () {
+    if (is_in_array(this.cases_noires, this.position_actuelle)){
+      this.cases_noires.pop(this.position_actuelle);
+    } else {
+      this.cases_noires.push(this.position_actuelle);
   }
+  };
+};
+
+var is_in_array = function(array, element){
+    var string_of_array = JSON.stringify(array);
+    return string_of_array.includes(element)
 };
