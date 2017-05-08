2d90275eeffc44b067e1b060448a313f4d23e9ba
Yohann Bosco <yohann.bosco@gmail.com>
2017-05-08 23:10:25 +0200

    La fourmi change de direction en fonction de la couleur de sa case et sa direction précédente

--- spec/langton_spec.js
+++ spec/langton_spec.js
@@ -1,30 +1,41 @@
 describe( "La Fourmi de Langton", function () {
   it("est sur une case blanche, dirigée vers le Nord et se déplace à l'Est", function () {
-    var fourmi = new Fourmi([3, 4], "Nord", []);
+    var fourmi = new Fourmi([3, 4], [0,1], []);
     fourmi.avance()
     expect(fourmi.position_actuelle).toEqual([3, 5]);
   });
-  it("est sur une case blanche, dirigée vers le Nord et colore la case en noir et se dirige à l'Est", function () {
-    var fourmi = new Fourmi([3, 4], "Nord", []);
-    fourmi.avance()
-    expect(fourmi.direction_actuelle).toEqual("Est");
-    expect(fourmi.cases_noires).toEqual([[3,4]]);
-  });
-  it("est sur une case noire, dirigée vers l'Ouest et colore la case en blanc et se dirige au Sud", function () {
-    var fourmi = new Fourmi([3, 4], "Ouest", [[3, 4]]);
-    fourmi.avance()
-    expect(fourmi.position_actuelle).toEqual([4, 4]);
-    expect(fourmi.direction_actuelle).toEqual("Sud");
-    expect(fourmi.cases_noires).toEqual([]);
-  });
   it("une case noire devient blanche", function () {
-    var fourmi = new Fourmi([3, 4], "Ouest", [[3, 4]]);
+    var fourmi = new Fourmi([3, 4], [-1, 0], [[3, 4]]);
     fourmi.change_couleur()
       expect(fourmi.cases_noires).toEqual([]);
   });
   it("une case blanche devient noire", function () {
-    var fourmi = new Fourmi([3, 4], "Ouest", []);
+    var fourmi = new Fourmi([3, 4], [-1, 0], []);
     fourmi.change_couleur()
       expect(fourmi.cases_noires).toEqual([[3, 4]]);
+  });
+  it("une fourmi sur case noire, tourne à  90° vers la gauche", function () {
+    var fourmi = new Fourmi([3, 4], [-1, 0], []);
+    fourmi.change_direction()
+      expect(fourmi.direction_actuelle).toEqual([0, 1]);
+  });
+  it("une fourmi sur case blanche, tourne à  90° vers la droite", function () {
+    var fourmi = new Fourmi([3, 4], [-1, 0], []);
+    fourmi.change_direction()
+      expect(fourmi.direction_actuelle).toEqual([0, 1]);
+  });
+  it("est sur une case blanche, dirigée vers le Nord et colore la case en noir et se dirige à l'Est", function () {
+    var fourmi = new Fourmi([3, 4], [0, 1], []);
+    fourmi.avance()
+    expect(fourmi.position_actuelle).toEqual([3, 5]);
+    expect(fourmi.direction_actuelle).toEqual([1, 0]);
+    expect(fourmi.cases_noires).toEqual([[3,4]]);
+  });
+  it("est sur une case noire, dirigée vers l'Ouest et colore la case en blanc et se dirige au Sud", function () {
+    var fourmi = new Fourmi([3, 4], [-1, 0], [[3, 4]]);
+    fourmi.avance()
+    expect(fourmi.position_actuelle).toEqual([4, 4]);
+    expect(fourmi.direction_actuelle).toEqual([0, -1]);
+    expect(fourmi.cases_noires).toEqual([]);
   });
 });
--- src/langton.js
+++ src/langton.js
@@ -4,19 +4,34 @@ function Fourmi (position_initiale, direction_initiale, cases_noires) {
   this.cases_noires = cases_noires;
 
   this.avance = function () {
+        this.change_couleur();
         this.position_actuelle = [3, 5];
-        this.direction_actuelle = "Est";
-        this.cases_noires = [[3, 4]];
+        this.direction_actuelle = [1, 0];
   };
 
   this.change_couleur = function () {
     if (is_in_array(this.cases_noires, this.position_actuelle)){
       this.cases_noires.pop(this.position_actuelle);
     } else {
-      this.cases_noires.push(this.position_actuelle);
+     this.cases_noires.push(this.position_actuelle);
   }
   };
+
+  this.change_direction = function () {
+    var tableau_conversion = {
+      "Ouest" : [-1, 0],
+      "Nord": [0, 1],
+      "Est": [1, 0],
+      "Sud": [0, -1]
+    };
+    if (is_in_array(this.cases_noires, this.position_actuelle)){
+      this.direction_actuelle =[this.direction_actuelle[1]*-1, this.direction_actuelle[0]*1];
+    } else {
+      this.direction_actuelle = [this.direction_actuelle[1]*1, this.direction_actuelle[0]*-1];
+    }
+  };
 };
+
 
 var is_in_array = function(array, element){
     var string_of_array = JSON.stringify(array);
