312eb28ad6ef374b5699e75c4e3e4c79ad3399d6
Simon <sims38930@gmail.com>
2017-06-02 12:23:45 +0200

    On pose la solution

--- R2N.py
+++ R2N.py
@@ -3,6 +3,13 @@ def convert(roman):
     dico = {"I":1, "V":5, "X":10}
     result = 0
 
-    for index, letter in enumerate(roman):
-        result += dico[letter]
+    previous_value = 0
+    for letter in reversed(roman):
+        current_value = dico[letter]
+        if current_value < previous_value:
+            result -= current_value
+        else:
+            result += current_value
+        previous_value = current_value
+
     return result
