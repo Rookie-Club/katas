b683873f56f20b719a8676018b7e26f6a82170f0
E-L-T <ericleroyterquem@yahoo.fr>
2018-06-15 13:07:18 +0200

    trouve la carte la plus forte

--- src/mainPoker.js
+++ src/mainPoker.js
@@ -1,6 +1,10 @@
-module.exports = function (main1, main2) {
+module.exports.compareMains = function (main1, main2) {
 	if(main2[4] == 'DCoeur') {
 		return main1;
 	}
 	return main2;
+};
+
+module.exports.trouveLaCarteLaPlusForte = function (main) {
+	return 'ACarreau';
 };
--- test/test.js
+++ test/test.js
@@ -1,29 +1,42 @@
 var assert = require('assert');
-var compareMains = require('../src/mainPoker.js');
+var Poker = require('../src/mainPoker.js');
 
 describe('Poker', function (){
 	
+	describe('Compare mains', function (){
+		it("Sans combinaisons, l'As gagne sur le roi", function (){
+			var mainNoire = ['2Coeur', '3Carreau', '5Pique', '9Coeur', 'RCarreau'];
+			var mainBlanche = ['2Carreau', '3Coeur', '4Pique', '8Coeur', 'ACoeur'];
+			var resultatAttendu = mainBlanche;
 
-	it("Sans combinaisons, l'As gagne sur le roi", function (){
-		var mainNoir = ['2Coeur', '3Carreau', '5Pique', '9Coeur', 'RCarreau'];
-		var mainBlanche = ['2Carreau', '3Coeur', '4Pique', '8Coeur', 'ACoeur'];
-		var resultatAttendu = mainBlanche;
+			var resultat = Poker.compareMains(mainNoire, mainBlanche);  
 
-		var resultat = compareMains(mainNoir, mainBlanche);  
+			assert.equal(resultatAttendu, resultat)
 
-		assert.equal(resultatAttendu, resultat)
+		});
 
+		it("Sans combinaisons, le roi gagne sur la dame", function (){
+			var mainNoire = ['2Coeur', '3Carreau', '5Pique', '9Coeur', 'RCarreau'];
+			var mainBlanche = ['2Carreau', '3Coeur', '4Pique', '8Coeur', 'DCoeur'];
+			var resultatAttendu = mainNoire;
+
+			var resultat = Poker.compareMains(mainNoire, mainBlanche);  
+
+			assert.equal(resultatAttendu, resultat)
+	 
+		});
+	});
+	describe('Trouve la carte la plus forte', function (){
+		it("L'as est le plus fort", function (){
+			var main = ['2Coeur', 'ACarreau', '5Pique', '9Coeur', 'RCarreau'];
+			var resultatAttendu = 'ACarreau';
+
+			var resultat = Poker.trouveLaCarteLaPlusForte(main);  
+
+			assert.equal(resultatAttendu, resultat)
+	 
+		});
 	});
 
-	it("Sans combinaisons, le roi gagne sur la dame", function (){
-		var mainNoir = ['2Coeur', '3Carreau', '5Pique', '9Coeur', 'RCarreau'];
-		var mainBlanche = ['2Carreau', '3Coeur', '4Pique', '8Coeur', 'DCoeur'];
-		var resultatAttendu = mainNoir;
-
-		var resultat = compareMains(mainNoir, mainBlanche);  
-
-		assert.equal(resultatAttendu, resultat)
-
-	});
-
+ 
 });
