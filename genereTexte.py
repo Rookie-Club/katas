ede8aaefdc21b9c5917aa1c9ddcf22112c435b34
Eric Leroy-Terquem <eric@captive.fr>
2018-07-13 17:51:16 +0200

    Calcul la grille des mots
    co-author: E-L-T <ericleroyterquem@yahoo.fr>
    
    co-author: emillumine <emillumine@gmail.com>

--- genereTexte.py
+++ genereTexte.py
@@ -1,21 +1,24 @@
-import sqlite3
+def genere_texte(texte, nombre_de_mots):
+  return un_texte_a_partir_de(nombre_de_mots, liste_de_trios_issu_de(texte))
 
-conn = sqlite3.connect('mots.sqlite3')
+def liste_de_trios_issu_de(texte):
+  trios = []
+  tableau_de_mots = texte.split()
+  tableau_de_mots.append('')
 
-cursor = conn.cursor()
-cursor.execute("""
-SELECT * FROM mots
-""")
+  taille_du_tableau = len(tableau_de_mots) - 1
+  index = 0
 
-mots = cursor.fetchone()
-print(mots)
+  while index < taille_du_tableau:
+    mot_suivant = tableau_de_mots[index + 1]
+    mot_courant = tableau_de_mots[index]
+    trios.append([mot_courant, mot_suivant, 1])
+    index += 1
+  return trios
 
-cursor.next()
-mots = cursor.fetchone()
-print(mots)
-cursor.next()
+def un_texte_a_partir_de(nombre_de_mots, liste_de_trios):
+  mots = []
+  for trio in liste_de_trios:
+    mots.append(trio[0])
 
-mots = cursor.fetchone()
-print(mots)
-
-conn.close()
+  return ' '.join(mots[0:nombre_de_mots])
