20d85c710a95141142f2236070e0c64c38401cb4
will <willi.am.publi@gmail.com>
2018-09-14 12:46:52 +0200

    refactoring selon les regles du functional calisthenics
    https://codurance.com/2017/10/12/functional-calisthenics

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,8 @@
 def retourne_fizzbuzz(entree):
-	sortie = str(entree)
-	if entree % 15 == 0:
-		sortie = "fizzbuzz"
-	elif entree % 3 == 0:
-		sortie = "fizz"
-	elif entree % 5 == 0:
-		sortie = "buzz"
-	return sortie
+	return "fizzbuzz" if entree % 15 == 0 else retourne_fizz(entree)
+
+def retourne_fizz(entree):
+	return "fizz" if entree % 3 == 0 else retourne_buzz(entree)
+
+def retourne_buzz(entree):
+	return "buzz" if entree % 5 == 0 else str(entree)
