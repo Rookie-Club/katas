76dec523a0c6d4aee0ccc686133cf7313f584306
jibe-b <user701@orange.fr>
2018-11-16 12:20:28 +0100

    Add increase and decrease quality function

--- gilded_rose.py
+++ gilded_rose.py
@@ -10,31 +10,36 @@ class GildedRose(object):
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
                     if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                        self.decrease_item_quality(item)
             else:
                 if item.quality < 50:
-                    item.quality = item.quality + 1
+                    self.increase_item_quality(item)
                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
                         if item.sell_in < 11:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                self.increase_item_quality(item)
                         if item.sell_in < 6:
                             if item.quality < 50:
-                                item.quality = item.quality + 1
+                                self.increase_item_quality(item)
             if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
+                item.sell_in -= 1
             if item.sell_in < 0:
                 if item.name != "Aged Brie":
                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
                         if item.quality > 0:
                             if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                                self.decrease_item_quality(item)
                     else:
                         item.quality = item.quality - item.quality
                 else:
                     if item.quality < 50:
-                        item.quality = item.quality + 1
+                        self.increase_item_quality(item)
+			    
+    def increase_item_quality(self, item):
+        item.quality += 1
 
+    def decrease_item_quality(self, item):
+        item.quality -= 1
 
 class Item:
     def __init__(self, name, sell_in, quality):
